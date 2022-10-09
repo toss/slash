220fade800dfed5964e8eaa2b36f238f3c70e56c
365kim <365kim@woowahan.com>
2022-10-09 23:27:28 +0900

    docs: fix broken links - packages/utils -> packages/common/utils

--- packages/react/use-query-param/src/hooks/useQueryParams.ts
+++ packages/react/use-query-param/src/hooks/useQueryParams.ts
@@ -25,7 +25,7 @@ export function useQueryParams<T extends { [key: string]: string } = { [key: str
   return useMemo(() => {
     /** router.query, QS.parse에서 사용하는 타입인 ParsedUrlQuery가
      * 유효한 제네릭을 받지 않아 any로 처리합니다
-     * packages/utils/src/queryString.ts 참고*/
+     * packages/common/utils/src/queryString.ts 참고*/
     return router.query as any;
   }, [router]);
 }
