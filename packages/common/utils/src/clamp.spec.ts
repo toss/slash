79d6ac580e7d8ce4ac3d89c3dd0efab9cce1d4a5
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-10-12 19:27:02 +0900

    fix: error handling in clamp function @tos/utils

--- packages/common/utils/src/clamp.spec.ts
+++ packages/common/utils/src/clamp.spec.ts
@@ -1,17 +1,23 @@
 import { clamp } from './clamp';
 
-describe('clamp는', () => {
-  it('max 만 있을 때 잘 동작한다', () => {
+describe('clamp should', () => {
+  it('work well when only given max value', () => {
     expect(clamp(3, 5)).toBe(3);
     expect(clamp(10, 6)).toBe(6);
     expect(clamp(6, 10)).toBe(6);
   });
 
-  it('min, max 모두 있을 때 잘 동작한다', () => {
+  it('work well when both value was given', () => {
     expect(clamp(3, 5, 10)).toBe(5);
     expect(clamp(10, 6, 10)).toBe(10);
     expect(clamp(6, 10, 10)).toBe(10);
     expect(clamp(7, 5, 10)).toBe(7);
     expect(clamp(100, 5, 6)).toBe(6);
+  });
+
+  it('throw error when bound1 is bigger than bound2', () => {
+    expect(() => clamp(3, 10, 5)).toThrowError();
+    expect(() => clamp(7, 12, 5)).toThrowError();
+    expect(() => clamp(100, 6, 5)).toThrowError();
   });
 });
--- packages/common/utils/src/clamp.ts
+++ packages/common/utils/src/clamp.ts
@@ -12,5 +12,9 @@ export function clamp(value: number, bound1: number, bound2?: number) {
     return Math.min(value, bound1);
   }
 
+  if (bound2 < bound1) {
+    throw new Error('The value of bound2 must be a number greater than bound1.');
+  }
+
   return Math.min(Math.max(value, bound1), bound2);
 }
