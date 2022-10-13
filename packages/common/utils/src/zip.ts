876227cc5c21e7ce992205b5f218c9df0c98c438
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-10-13 20:58:25 +0900

    docs: fill in missing @description of zip

--- packages/common/utils/src/zip.ts
+++ packages/common/utils/src/zip.ts
@@ -3,7 +3,16 @@ type Zipped<T extends unknown[][]> = Array<{ [Key in keyof T]: ElementOf<T[Key]>
 
 /**
  * @name zip
+ * @description
  * Array의 zip 연산을 수행합니다.
+ *
+ * ```typescript
+ * zip<T extends unknown[][]>(
+ *   // zip 연산이 수행될 배열
+ *   ...arrays: [...T]
+ * ): Zipped<T>
+ * ```
+ *
  * @example
  * zip([1, 2], ['a', 'b']) --> [[1, 'a'], [2, 'b']]
  */
