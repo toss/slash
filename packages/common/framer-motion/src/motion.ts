1051cca9fbcf8da3eced204f65da603b1ea400ab
JeonIJin <pongpong0121@kakao.com>
2022-10-14 14:56:51 +0900

    docs(framer-motion): support i18n and add code blocks (#51)
    * feat: add @docusaurus/theme-live-codeblock
    
    * fix: framer-motion docs with i18n and Code blocks
    
    * revert: remove live editor
    
    * chore: remove theme file
    
    * docs: add link of framer-motion

--- packages/common/framer-motion/src/motion.ts
+++ packages/common/framer-motion/src/motion.ts
@@ -1,19 +1,6 @@
+/** @tossdocs-ignore */
 import { createDomMotionComponent } from 'framer-motion';
 
-/**
- * @name motion
- * @description
- * 기본 `framer-motion`의 `motion` 은 Internet Explorer 등 구형 브라우저를 지원하지 않아서 지원하도록 Wrapping 한 컴포넌트입니다.
- * 사용 방법은 기본 motion 과 동일합니다.
- *
- * 원래 `motion` 컴포넌트는 `Proxy` 라고 하는 Polyfill 불가능한 API를 사용하기 때문에, 구형 브라우저에서 실행할 수 없습니다.
- * 보다 넓은 범위의 기기를 지원하기 위해서 `@toss/framer-motion` 의 `motion` 컴포넌트를 사용해주세요.
- *
- * @example
- * import { motion } from '@toss/framer-motion';`
- *`
- * <motion.div {...} />
- */
 export const motion = {
   div: createDomMotionComponent('div'),
   span: createDomMotionComponent('span'),
