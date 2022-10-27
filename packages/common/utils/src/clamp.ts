7c67c5c76a8cc71ae4d72268eff02d6d1676429a
HoseungJang <hsjang.dev@gmail.com>
2022-10-27 22:51:53 +0900

    utils 일부 문서화

--- packages/common/utils/src/clamp.ts
+++ packages/common/utils/src/clamp.ts
@@ -1,12 +1,4 @@
-/**
- * @name clamp
- * @description `value`가 bound안에 존재하면 value를, 아니라면 value와 가까운 bound를 반환(bound1: 최소, bound2: 최대)
- * @example
- * clamp(3, 1) // 3
- * clamp(3, 1, 5) // 3
- * clamp(3, 5) // 5
- * clamp(7, 3, 5) // 5
- */
+/** @tossdocs-ignore */
 export function clamp(value: number, bound1: number, bound2?: number) {
   if (bound2 == null) {
     return Math.min(value, bound1);
--- packages/common/utils/src/device/isMacOS.ts
+++ packages/common/utils/src/device/isMacOS.ts
@@ -1,13 +1,6 @@
+/** @tossdocs-ignore */
 import { isServer } from './isServer';
 
-/**
- * @name isMacOS
- * @description
- * Mac OS 환경에서 실행된 JS 런타임인지 확인합니다.
- * ```typescript
- * isMacOS(): boolean;
- * ```
- */
 export function isMacOS() {
   if (isServer()) {
     return false;
