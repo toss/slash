12835ffeec999226d3938dc9bbf2f54125d24c92
201411108 <aggsae@gmail.com>
2022-10-30 12:29:41 +0900

    fix(utils): fix floating point comma

--- packages/common/utils/src/Numbers.ts
+++ packages/common/utils/src/Numbers.ts
@@ -74,7 +74,13 @@ export function formatToKRW(
 }
 
 export function commaizeNumber(value: string | number) {
-  return String(value).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+  const numStr = String(value);
+  const decimalPointIndex = numStr.indexOf('.');
+  const commaizeRegExp = /(\d)(?=(\d\d\d)+(?!\d))/g;
+
+  return decimalPointIndex > -1
+    ? numStr.slice(0, decimalPointIndex).replace(commaizeRegExp, '$1,') + numStr.slice(decimalPointIndex)
+    : numStr.replace(commaizeRegExp, '$1,');
 }
 
 export function floorAndFormatNumber(value: number) {
