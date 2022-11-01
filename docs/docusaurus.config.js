d8f3614d4a69e7639609e1a57c98a1d55d05f520
raon0211 <raon0211@toss.im>
2022-11-01 21:23:26 +0900

    Add algolia

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -99,7 +99,6 @@ module.exports = {
         apiKey: process.env.ALGOLIA_API_KEY,
         siteId: 'f93364cf-0ae8-4398-b7bd-9a9f582c78ac',
         branch: 'main',
-        selector: 'div#search',
         contextualSearch: true,
       },
     },
