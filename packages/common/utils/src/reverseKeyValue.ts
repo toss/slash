4072aed5b68369045f63ee48b7de5c97f85268eb
박찬혁 <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2022-11-04 00:04:03 +0900

    docs: @toss/utils remove duplicated docs (#115)

--- packages/common/utils/src/reverseKeyValue.ts
+++ packages/common/utils/src/reverseKeyValue.ts
@@ -1,3 +1,4 @@
+/** @tossdocs-ignore */
 export function reverseKeyValue<KeyType extends string, ValueType extends string>(obj: Record<KeyType, ValueType>) {
   return Object.fromEntries(Object.entries(obj).map(([key, value]) => [value, key])) as Record<ValueType, KeyType>;
 }
