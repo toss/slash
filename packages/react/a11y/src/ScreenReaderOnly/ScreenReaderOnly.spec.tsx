35751a0f058651993d9446cba4a2f11a98f0d02c
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-11-04 13:21:54 +0900

    feat: @toss/a11y write test code

--- packages/react/a11y/src/ScreenReaderOnly/ScreenReaderOnly.spec.tsx
+++ packages/react/a11y/src/ScreenReaderOnly/ScreenReaderOnly.spec.tsx
@@ -1,19 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ScreenReaderOnly } from '.';
-
-const renderFixture = (testId = 'fixture') => {
-  const { getByTestId } = render(
-    <ScreenReaderOnly data-testid={testId}>
-      <h1>토스</h1>
-    </ScreenReaderOnly>
-  );
-
-  return getByTestId(testId);
-};
 
 describe('ScreenReaderOnly', () => {
   it('should have proper style', () => {
-    const fixture = renderFixture();
+    const { getByTestId } = render(<ScreenReaderOnly data-testid="fixture">토스</ScreenReaderOnly>);
+    const fixture = getByTestId('fixture');
 
     expect(fixture).toHaveStyle({
       position: 'absolute',
@@ -25,5 +16,27 @@ describe('ScreenReaderOnly', () => {
       overflow: 'hidden',
       whiteSpace: 'nowrap',
     });
+
+    expect(fixture).not.toHaveStyle({
+      position: 'relative',
+    });
+  });
+
+  it('should render children well', () => {
+    render(<ScreenReaderOnly>토스</ScreenReaderOnly>);
+
+    expect(screen.getByText('토스')).toBeInTheDocument();
+    expect(screen.queryByText('toss')).not.toBeInTheDocument();
+  });
+
+  it('should receive other props', () => {
+    const { getByTestId } = render(
+      <ScreenReaderOnly color="white" data-testid="fixture">
+        토스
+      </ScreenReaderOnly>
+    );
+    const fixture = getByTestId('fixture');
+    expect(fixture.getAttribute('color')).toEqual('white');
+    expect(fixture.getAttribute('color')).not.toEqual('black');
   });
 });
