e997a43a5cfebbd0531c2f3f76ebc8fbbdc40f27
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-11-04 12:44:14 +0900

    feat: @toss/a11y verify proper style

--- packages/react/a11y/src/ScreenReaderOnly/ScreenReaderOnly.spec.tsx
+++ packages/react/a11y/src/ScreenReaderOnly/ScreenReaderOnly.spec.tsx
@@ -1,14 +1,29 @@
 import { render } from '@testing-library/react';
 import { ScreenReaderOnly } from '.';
 
+const renderFixture = (testId = 'fixture') => {
+  const { getByTestId } = render(
+    <ScreenReaderOnly data-testid={testId}>
+      <h1>토스</h1>
+    </ScreenReaderOnly>
+  );
+
+  return getByTestId(testId);
+};
+
 describe('ScreenReaderOnly', () => {
   it('should have proper style', () => {
-    const result = render(
-      <ScreenReaderOnly>
-        <h1>토스</h1>
-      </ScreenReaderOnly>
-    );
+    const fixture = renderFixture();
 
-    expect(result.getByText('토스')).toBeInTheDocument();
+    expect(fixture).toHaveStyle({
+      position: 'absolute',
+      width: '1px',
+      height: '1px',
+      padding: '0px',
+      margin: '-1px',
+      borderWidth: '0px',
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+    });
   });
 });
