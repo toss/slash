4f906b0dc5beccd00ba4b14a87993e86db4aacc7
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-11-07 09:35:24 +0900

    fix: do not use test-id it is last resort

--- packages/react/a11y/src/ScreenReaderOnly/ScreenReaderOnly.spec.tsx
+++ packages/react/a11y/src/ScreenReaderOnly/ScreenReaderOnly.spec.tsx
@@ -3,8 +3,8 @@ import { ScreenReaderOnly } from '.';
 
 describe('ScreenReaderOnly', () => {
   it('should have proper style', () => {
-    const { getByTestId } = render(<ScreenReaderOnly data-testid="fixture">토스</ScreenReaderOnly>);
-    const fixture = getByTestId('fixture');
+    const { getByText } = render(<ScreenReaderOnly>토스</ScreenReaderOnly>);
+    const fixture = getByText('토스');
 
     expect(fixture).toHaveStyle({
       position: 'absolute',
@@ -30,12 +30,8 @@ describe('ScreenReaderOnly', () => {
   });
 
   it('should receive other props', () => {
-    const { getByTestId } = render(
-      <ScreenReaderOnly color="white" data-testid="fixture">
-        토스
-      </ScreenReaderOnly>
-    );
-    const fixture = getByTestId('fixture');
+    const { getByText } = render(<ScreenReaderOnly color="white">토스</ScreenReaderOnly>);
+    const fixture = getByText('토스');
     expect(fixture.getAttribute('color')).toEqual('white');
     expect(fixture.getAttribute('color')).not.toEqual('black');
   });
