f3b501ab7b0461935bd0d0f9ce3f513cd59aa9d6
박찬혁 <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2022-11-14 10:46:49 +0900

    chore(utils): Move `isDifferentArray` to `src/array` (#138)
    * chore: set the test environment for a11y
    
    * chore: set the test environment for a11y
    
    * feat: @toss/a11y verify proper style
    
    * feat: @toss/a11y write test code
    
    * fix: @toss/a11y replace default export
    
    * fix: do not use test-id it is last resort
    
    * fix: use more proper attribute
    
    * fix: replace isDifferentArray locate

--- packages/common/utils/src/array/index.ts
+++ packages/common/utils/src/array/index.ts
@@ -1,6 +1,8 @@
 /** @tossdocs-ignore */
 export * from './arrayIncludes';
+export * from './isDifferentArray';
 export * from './isNonEmptyArray';
 export * from './last';
 export * from './NonEmptyArray';
 export * from './sample';
+
