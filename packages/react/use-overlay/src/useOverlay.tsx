d989a1cb942b26239eb89717e2e1c35027188b16
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-11-16 14:23:12 +0900

    fix: revert key of OverlayController

--- packages/react/use-overlay/src/useOverlay.tsx
+++ packages/react/use-overlay/src/useOverlay.tsx
@@ -36,7 +36,7 @@ export function useOverlay({ exitOnUnmount = true }: Options = {}) {
         mount(
           id,
           <OverlayController
-            key={id}
+            key={Date.now()}
             ref={overlayRef}
             overlayElement={overlayElement}
             onExit={() => {
