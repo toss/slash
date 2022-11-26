ba03ae804c01f299291583f57451b47bba783c7c
Jonghyeon Ko <manudeli.ko@gmail.com>
2022-11-26 19:42:08 +0900

    feat(ResetKey): add withResetKey.Provider/Consumer

--- packages/react/error-boundary/src/contexts/ResetKey.tsx
+++ packages/react/error-boundary/src/contexts/ResetKey.tsx
@@ -1,34 +1,24 @@
-import {
-  ComponentProps,
-  ComponentType,
-  createContext,
-  PropsWithChildren,
-  useCallback,
-  useContext,
-  useState,
-} from 'react';
+import { ComponentProps, ComponentType, createContext, ReactNode, useContext } from 'react';
+import { useKey } from '../hooks';
 
-const ResetKeyContext = createContext<{ resetKey: number; reset: () => void }>({ resetKey: 0, reset: () => {} });
+const ResetKeyContext = createContext({ resetKey: {}, reset: () => {} });
 if (process.env.NODE_ENV !== 'production') {
   ResetKeyContext.displayName = 'ResetKeyContext';
 }
 
-export const ResetKeyProvider = ({ children }: PropsWithChildren) => {
-  const [resetKey, setResetKey] = useState(0);
-  const reset = useCallback(() => setResetKey(prev => prev + 1), []);
+export const ResetKeyProvider = (props: { children: ReactNode }) => {
+  const [resetKey, reset] = useKey();
 
-  return <ResetKeyContext.Provider value={{ resetKey, reset }}>{children}</ResetKeyContext.Provider>;
+  return <ResetKeyContext.Provider {...props} value={{ reset, resetKey }} />;
 };
 
-export const ResetKeyConsumer = (props: { children: ComponentProps<typeof ResetKeyContext.Consumer>['children'] }) => (
-  <ResetKeyContext.Consumer {...props} />
-);
+export const ResetKeyConsumer = ResetKeyContext.Consumer;
 
 export const useResetKey = () => useContext(ResetKeyContext);
 
-export const withResetKey =
+const withResetKeyProviderConsumer =
   <P extends Record<string, unknown> = Record<string, never>>(
-    Component: ComponentType<ReturnType<typeof useResetKey> & P>
+    Component: ComponentType<Parameters<ComponentProps<typeof ResetKeyConsumer>['children']>[0] & P>
   ) =>
   (props: P) =>
     (
@@ -38,3 +28,31 @@ export const withResetKey =
         </ResetKeyConsumer>
       </ResetKeyProvider>
     );
+
+const withResetKeyProvider =
+  <P extends Record<string, unknown> = Record<string, never>>(Component: ComponentType<P>) =>
+  (props: P) =>
+    (
+      <ResetKeyProvider>
+        <Component {...props} />
+      </ResetKeyProvider>
+    );
+
+const withResetKeyConsumer =
+  <P extends Record<string, unknown> = Record<string, never>>(
+    Component: ComponentType<Parameters<ComponentProps<typeof ResetKeyConsumer>['children']>[0] & P>
+  ) =>
+  (props: P) =>
+    (
+      <ResetKeyConsumer>
+        {({ reset, resetKey }) => <Component reset={reset} resetKey={resetKey} {...props} />}
+      </ResetKeyConsumer>
+    );
+
+export const withResetKey = withResetKeyProviderConsumer as typeof withResetKeyProviderConsumer & {
+  Provider: typeof withResetKeyProvider;
+  Consumer: typeof withResetKeyConsumer;
+};
+
+withResetKey.Provider = withResetKeyProvider;
+withResetKey.Consumer = withResetKeyConsumer;
---
+++ packages/react/error-boundary/src/hooks/index.ts
@@ -0,0 +1,1 @@
+export { useKey } from './useKey';
