7538184655a9c023f35b6fe64c9e46386e5a10f2
박찬혁 <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2022-12-01 15:38:56 +0900

    feat(utils): Allow generic argument in `get` (#154)
    * docs: get method return type
    
    * fix: open generic type in get function
    
    Co-authored-by: Sojin Park <raon0211@toss.im>

--- packages/common/utils/src/get-set.ts
+++ packages/common/utils/src/get-set.ts
@@ -1,12 +1,12 @@
 /** @tossdocs-ignore */
-export const get = (obj: Record<string, any>, path: string, defaultValue = undefined) => {
+export const get = <T = any>(obj: Record<string, any>, path: string, defaultValue?: T): T => {
   const travel = (regexp: RegExp) =>
     String.prototype.split
       .call(path, regexp)
       .filter(Boolean)
       .reduce((acc, key) => (acc !== null && acc !== undefined ? acc[key] : acc), obj);
   const result = travel(/[,[\]]+?/) || travel(/[,[\].]+?/);
-  return result === undefined || result === obj ? defaultValue : result;
+  return (result === undefined || result === obj ? defaultValue : result) as T;
 };
 
 export const set = (obj: Record<string, any>, path: string, value: any) => {
--- packages/common/utils/src/get-set_get.ko.md
+++ packages/common/utils/src/get-set_get.ko.md
@@ -6,15 +6,14 @@ sidebar_label: get
 객체의 특정 경로에 있는 값을 반환합니다.
 
 ```typescript
-function get(
+function get<T = any>(
   // 값을 가져올 객체
   obj: Record<string, any>,
   // 가져올 값의 경로
   path: string,
   // 경로에 해당하는 값이 없을 때 반환할 기본값
-  // @default undefined
-  defaultValue?: any
-): any;
+  defaultValue?: T
+): T;
 ```
 
 ## Example
@@ -22,4 +21,5 @@ function get(
 ```typescript
 get({ a: { b: 1 } }, 'a.b'); // 1
 get({ a: { b: 1 } }, 'a.b.c'); // undefined
+get<boolean>({ a: { b: true } }, 'a.b.c', false); // false
 ```
