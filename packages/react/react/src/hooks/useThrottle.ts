3fe4d0a9bf19c93f7760514e8f8a649f25f49081
HYUNGU KANG <69349293+LineGu@users.noreply.github.com>
2023-01-03 18:32:47 +0900

    fix(react): fix @toss/react to support esm (#179)
    * fix(react): fix @toss/react to support esm
    
    * fix: fix @toss/react to support esm - lodash throttle
    
    Co-authored-by: goo <goo@tossbank.com>

--- packages/react/react/src/hooks/useThrottle.ts
+++ packages/react/react/src/hooks/useThrottle.ts
@@ -1,4 +1,4 @@
-import throttle from 'lodash/throttle';
+import throttle from 'lodash.throttle';
 import { useEffect, useMemo } from 'react';
 import { usePreservedCallback } from './usePreservedCallback';
 import { usePreservedReference } from './usePreservedReference';
