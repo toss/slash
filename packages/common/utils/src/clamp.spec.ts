1a5c0e638e6b67e2a37fe41684fc76dccd2c85ba
Minsu Kim <alstn2468_@naver.com>
2023-01-04 17:33:38 +0900

    fix: fix another test case when max value is null

--- packages/common/utils/src/clamp.spec.ts
+++ packages/common/utils/src/clamp.spec.ts
@@ -3,8 +3,8 @@ import { clamp } from './clamp';
 describe('clamp', () => {
   it('should work well when only given max value', () => {
     expect(clamp(3, 5)).toBe(5);
-    expect(clamp(10, 6)).toBe(6);
-    expect(clamp(6, 10)).toBe(6);
+    expect(clamp(10, 6)).toBe(10);
+    expect(clamp(6, 10)).toBe(10);
   });
 
   it('should work well when both value was given', () => {
