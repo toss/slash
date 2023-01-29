6368ba6f35719b08932951ca75376e3768b3c1aa
퉁이리 <wer4272@nate.com>
2023-01-30 04:26:45 +0900

    docs: useForceUpdate jsdoc @link
    Co-authored-by: Jonghyeon Ko <jonghyeon.ko@wesang.com>

--- packages/react/react/src/hooks/useForceUpdate.ts
+++ packages/react/react/src/hooks/useForceUpdate.ts
@@ -7,7 +7,7 @@ const updater = (num: number): number => (num + 1) % 1_000_000;
  * @description
  * 반환된 함수를 실행 시 강제로 리렌더가 실행됩니다.
  *
- * @note useReduce가 state보다 가볍다는 의견이 있습니다. https://github.com/streamich/react-use/pull/837
+ * @note {@link https://github.com/streamich/react-use/pull/837 useReduce가 state보다 가볍다는 의견이 있습니다.}
  */
 export function useForceUpdate(): () => void {
   const [, forceUpdate] = useReducer(updater, 0);
