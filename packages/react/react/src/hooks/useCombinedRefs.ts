dd1a89d302be96200db678ec8da83f3f12181db0
il <wer4272@nate.com>
2023-02-02 07:09:50 +0900

    fix(react): usecombinedrefs-docs

--- packages/react/react/src/hooks/useCombinedRefs.ts
+++ packages/react/react/src/hooks/useCombinedRefs.ts
@@ -4,7 +4,7 @@ import { MutableRefObject, Ref, RefCallback, useCallback } from 'react';
  * @description 여러 개의 ref를 하나로 합치고 싶을 때 사용할 수 있는 hook입니다.
  *
  * ```ts
- * function useCombinedRefs<T>(...refs: Array<Ref<T>>): RefCallback<T> | null;
+ * function useCombinedRefs<T>(...refs: Array<Ref<T>>): RefCallback<T>;
  * ```
  *
  * @example
