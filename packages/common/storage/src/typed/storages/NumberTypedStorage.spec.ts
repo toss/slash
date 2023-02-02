b062036ac25ca6025c2b804889e25879e4ab32ff
chanhyukpark-29cm <chanhyuk-tech@29cm.co.kr>
2023-02-03 01:52:24 +0900

    fix: test description more sophisticated

--- packages/common/storage/src/typed/storages/NumberTypedStorage.spec.ts
+++ packages/common/storage/src/typed/storages/NumberTypedStorage.spec.ts
@@ -2,12 +2,12 @@ import { NumberTypedStorage } from './NumberTypedStorage';
 
 describe('NumberTypedStorage', () => {
   describe('increase() method', () => {
-    it('should increase the value of the given key by 1. when given no argument', () => {
+    it('should increase the value of the given key by 1', () => {
       const typed = new NumberTypedStorage('increase-test-no-argument', { initialValue: 10 });
       typed.increase();
       expect(typed.get()).toBe(11);
     });
-    it('should increase the value of the given key by argument.', () => {
+    it('should increase the value of the given key by offset', () => {
       const typed = new NumberTypedStorage('increase-test-with-argument', { initialValue: 10 });
       typed.increase(10);
       expect(typed.get()).toBe(20);
@@ -17,12 +17,12 @@ describe('NumberTypedStorage', () => {
   });
 
   describe('decrease() method', () => {
-    it('should decrease the value of the given key by 1. when given no argument', () => {
+    it('should decrease the value of the given key by 1', () => {
       const typed = new NumberTypedStorage('decrease-test-no-argument', { initialValue: 10 });
       typed.decrease();
       expect(typed.get()).toBe(9);
     });
-    it('should decrease the value of the given key by argument.', () => {
+    it('should decrease the value of the given key by offset', () => {
       const typed = new NumberTypedStorage('decrease-test-with-argument', { initialValue: 50 });
       typed.decrease(10);
       expect(typed.get()).toBe(40);
