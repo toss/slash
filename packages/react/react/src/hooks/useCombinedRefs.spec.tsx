c9fb8de12b116ff6930d04fe39a5e490566713d4
il <wer4272@nate.com>
2023-02-06 17:24:14 +0900

    revert: useCombinedRefs spec

--- packages/react/react/src/hooks/useCombinedRefs.spec.tsx
+++ packages/react/react/src/hooks/useCombinedRefs.spec.tsx
@@ -5,26 +5,24 @@ import { useCombinedRefs } from './useCombinedRefs';
 
 describe('useCombinedRefs', () => {
   it('여러 개의 ref를 하나로 합칠 수 있다.', async () => {
-    const refs: Array<HTMLDivElement | null | undefined> = [];
-    const refCallback = jest.fn();
+    const refs: Array<HTMLDivElement | null> = [];
+    const callbackRef = jest.fn();
 
     function TestComponent() {
-      const ref1 = useRef<HTMLDivElement>();
-      const ref2 = useRef();
-      const ref3 = useRef(null);
-      const ref4 = useRef<HTMLDivElement>(null);
-      const ref5 = useCallback((element: HTMLDivElement | null) => {
-        refCallback(element);
+      const ref1 = useRef<HTMLDivElement>(null);
+      const ref2 = useRef<HTMLDivElement>(null);
+      const ref3 = useCallback((element: HTMLDivElement | null) => {
+        callbackRef(element);
       }, []);
 
-      const ref = useCombinedRefs(ref1, ref2, ref3, ref4, ref5);
+      const ref = useCombinedRefs(ref1, ref2, ref3);
 
       return (
         <>
           <div ref={ref}>hi</div>
           <button
             onClick={() => {
-              refs.push(ref1.current, ref2.current, refCallback.mock.calls[0][0]);
+              refs.push(ref1.current, ref2.current, callbackRef.mock.calls[0][0]);
             }}
           >
             ref 저장하기
