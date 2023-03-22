99b9057241c206a3c8dbf4852360cc97c6865054
jeyog <choijy8816@gmail.com>
2023-03-22 20:19:20 +0900

    feat: add 호격조사(아/야) to josaOption

--- packages/common/hangul/src/josa.ts
+++ packages/common/hangul/src/josa.ts
@@ -2,7 +2,7 @@
 import { disassembleCompleteHangulCharacter } from './disassembleCompleteHangulCharacter';
 import { hasBatchim } from './utils';
 
-type JosaOption = '이/가' | '을/를' | '은/는' | '으로/로' | '와/과' | '이나/나' | '이에/에' | '이란/란';
+type JosaOption = '이/가' | '을/를' | '은/는' | '으로/로' | '와/과' | '이나/나' | '이에/에' | '이란/란' | '아/야';
 
 export function josa(word: string, josa: JosaOption): string {
   if (word.length === 0) {
