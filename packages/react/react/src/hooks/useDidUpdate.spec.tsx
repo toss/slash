ec6b3634038202ddda7943cd0d92ac6ffa6e5e31
원건우 <wongeon-u@wongunwoo.local>
2023-06-02 00:00:14 +0900

    test(@toss/react): wrote test code for useDidUpdate

---
+++ packages/react/react/src/hooks/useDidUpdate.spec.tsx
@@ -0,0 +1,52 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { useState } from 'react';
+import { useDidUpdate } from './useDidUpdate';
+
+const TOSS_TEXT = 'TOSS';
+
+interface ITestComponentProps {
+  effectFn: () => void;
+}
+
+describe('useDidUpdate', () => {
+  function TestingComponent({ effectFn }: ITestComponentProps) {
+    const [count, setCount] = useState<number>(0);
+    useDidUpdate(effectFn, [count]);
+
+    const onClickCountUp = () => {
+      setCount(count + 1);
+    };
+
+    return (
+      <button type="button" onClick={onClickCountUp}>
+        {TOSS_TEXT}
+      </button>
+    );
+  }
+  it('If get a rendering only once', () => {
+    const effectFn = jest.fn();
+
+    expect(effectFn).not.toHaveBeenCalled();
+
+    render(<TestingComponent effectFn={effectFn} />);
+    expect(effectFn).not.toHaveBeenCalled();
+  });
+
+  it('If rendering several times', () => {
+    const effectFn = jest.fn();
+
+    expect(effectFn).not.toHaveBeenCalled();
+
+    render(<TestingComponent effectFn={effectFn} />);
+    const button = screen.getByText(TOSS_TEXT);
+
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+
+    expect(effectFn).toHaveBeenCalled();
+
+    expect(effectFn).toHaveBeenCalledTimes(3);
+  });
+});
