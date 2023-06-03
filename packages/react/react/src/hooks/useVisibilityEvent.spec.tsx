108f51f59bfa0482d0e1cf35571b22ccc5632641
원건우 <wongeon-u@wongunwoo.local>
2023-06-03 18:09:40 +0900

    test(@toss/react): wrote test code for useVisibilityEvent

---
+++ packages/react/react/src/hooks/useVisibilityEvent.spec.tsx
@@ -0,0 +1,41 @@
+import { fireEvent, render, renderHook } from '@testing-library/react';
+import { useState } from 'react';
+import { useVisibilityEvent } from './useVisibilityEvent';
+
+type VisibilityState = Document['visibilityState'];
+
+describe('useVisibilityEvent', () => {
+  it('When a user leaves the page.', () => {
+    const callbackMock = jest.fn();
+    renderHook(() => useVisibilityEvent(callbackMock));
+    expect(document.visibilityState).toBe('visible');
+
+    Object.defineProperty(document, 'visibilityState', { value: 'hidden' });
+    fireEvent(document, new Event('visibilitychange'));
+
+    expect(document.visibilityState).toBe('hidden');
+  });
+});
+
+describe('useVisibilityEvent for component', () => {
+  it('When a user leaves the page (component).', () => {
+    function TestComponent() {
+      const [visitable, setVisitable] = useState('visible');
+      const visibleCallbackFunc = (value: VisibilityState) => {
+        setVisitable(value);
+      };
+      useVisibilityEvent(visibleCallbackFunc);
+      return <div>{visitable}</div>;
+    }
+    const { getByText } = render(<TestComponent />);
+
+    expect(getByText('visible')).toBeInTheDocument();
+
+    Object.defineProperty(document, 'visibilityState', { value: 'hidden' });
+    fireEvent(document, new Event('visibilitychange'));
+
+    expect(getByText('hidden')).toBeInTheDocument();
+
+    expect(document.visibilityState).toBe('hidden');
+  });
+});
