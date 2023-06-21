60e013b4a8777e07d8a6d604ddcc4882571a3bc9
ssi02014 <ssi02014@naver.com>
2023-06-22 03:17:11 +0900

    feat(@toss/react): useActionObserver Hook

---
+++ packages/react/react/src/hooks/useActionObserver.tsx
@@ -0,0 +1,53 @@
+import { usePreservedCallback } from './usePreservedCallback';
+import { useRefEffect } from './useRefEffect';
+
+interface UseActionObserverProps {
+  onAction: () => void;
+  threshold?: number | number[];
+  root?: Document | Element | null;
+  rootMargin?: string;
+  repetitionCount?: number;
+}
+
+/** @tossdocs-ignore */
+export function useActionObserver<E extends HTMLElement = HTMLElement>({
+  onAction,
+  threshold,
+  rootMargin,
+  repetitionCount,
+  root = null,
+}: UseActionObserverProps) {
+  const actionCallback = usePreservedCallback(onAction);
+  const ref = useRefEffect<E>(
+    elem => {
+      let callCount = 0;
+
+      const observer = new IntersectionObserver(
+        ([entry]: IntersectionObserverEntry[]) => {
+          if (repetitionCount != null && repetitionCount <= callCount) {
+            observer.unobserve(elem);
+            return;
+          }
+
+          if (entry.isIntersecting) {
+            callCount++;
+            actionCallback();
+          }
+        },
+        {
+          threshold,
+          root,
+          rootMargin,
+        }
+      );
+      observer.observe(elem);
+
+      return () => {
+        observer.unobserve(elem);
+      };
+    },
+    [actionCallback]
+  );
+
+  return ref;
+}
