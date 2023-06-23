d4edf9bcde5335c3e7b636d45636b38e7df2bf58
ssi02014 <ssi02014@naver.com>
2023-06-23 22:39:36 +0900

    fix(@toss/react): Fix Portal Component Test Code

--- packages/react/react/src/components/Portal/index.spec.tsx
+++ packages/react/react/src/components/Portal/index.spec.tsx
@@ -3,45 +3,41 @@ import { Portal } from './Portal';
 
 const TestComponent = ({ id }: { id: string }) => {
   return (
-    <div id="root">
-      <Portal id={id}>
-        <div id="child">Example Portal</div>
-      </Portal>
-    </div>
+    <>
+      <div id="parent">
+        <Portal id={id}>
+          <div id="child">Example Portal</div>
+        </Portal>
+      </div>
+      <div id="outer"></div>
+    </>
   );
 };
 
-beforeAll(() => {
-  const tempDiv = document.createElement('div');
-  tempDiv.setAttribute('id', 'outer');
-
-  document.body.appendChild(tempDiv);
-});
-
 describe('Test Portal Component', () => {
-  it('부모 컴포넌트 DOM 계층 구조 바깥에 추가한 DOM Node의 id와 같다면, 해당 Node의 자식으로 렌더링된다.', () => {
-    render(<TestComponent id="outer" />);
+  it("If the id of the DOM Node added outside the parent component's DOM hierarchy is the same, it is rendered as a child of that Node.", () => {
+    const { container } = render(<TestComponent id="outer" />);
 
-    const root = document.body.querySelector('#root');
-    const outerPortal = document.body.querySelector('#outer');
+    const parent = container.querySelector('#parent');
+    const outer = container.querySelector('#outer');
 
-    const rootChildren = root?.querySelector('#child');
-    const outerPortalChildren = outerPortal?.querySelector('#child');
+    const parentChildren = parent?.querySelector('#child');
+    const outerChildren = outer?.querySelector('#child');
 
-    expect(rootChildren).not.toBeInTheDocument();
-    expect(outerPortalChildren).toBeInTheDocument();
+    expect(parentChildren).not.toBeInTheDocument();
+    expect(outerChildren).toBeInTheDocument();
   });
 
-  it('부모 컴포넌트 DOM 계층 구조 바깥에 추가한 DOM Node의 id와 다르다면, 해당 Node의 자식으로 렌더링되지 않고 부모 컴포넌트의 자식으로 렌더링된다.', () => {
-    render(<TestComponent id="etc" />);
+  it("If the id of the DOM Node added outside of the parent component's DOM hierarchy is different, it will not be rendered as a child of that Node, but will be rendered as a child of the parent component.", () => {
+    const { container } = render(<TestComponent id="etc" />);
 
-    const root = document.body.querySelector('#root');
-    const outerPortal = document.body.querySelector('#outer');
+    const parent = container.querySelector('#parent');
+    const outer = container.querySelector('#outer');
 
-    const rootChildren = root?.querySelector('#child');
-    const outerPortalChildren = outerPortal?.querySelector('#child');
+    const parentChildren = parent?.querySelector('#child');
+    const outerChildren = outer?.querySelector('#child');
 
-    expect(rootChildren).toBeInTheDocument();
-    expect(outerPortalChildren).not.toBeInTheDocument();
+    expect(parentChildren).toBeInTheDocument();
+    expect(outerChildren).not.toBeInTheDocument();
   });
 });
