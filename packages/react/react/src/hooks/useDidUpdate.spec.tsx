6caf599e296af0bd976b83f63461d8f7320806c9
rjsdnql123 <61114705+rjsdnql123@users.noreply.github.com>
2023-07-03 14:55:15 +0900

    test(@toss/react): wrote test code for useDidUpdate (#244)
    * test(@toss/react): wrote test code for useDidUpdate
    
    * Modify: Description for useDidUpdate-test-code
    
    * Fix: interface phrases
    
    ---------
    
    Co-authored-by: 원건우 <wongeon-u@wongunwoo.local>
    Co-authored-by: Sojin Park <raon0211@toss.im>

---
+++ packages/react/react/src/hooks/useDidUpdate.spec.tsx
@@ -0,0 +1,52 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { useState } from 'react';
+import { useDidUpdate } from './useDidUpdate';
+
+const TOSS_TEXT = 'TOSS';
+
+interface TestComponentProps {
+  effectFn: () => void;
+}
+
+describe('useDidUpdate', () => {
+  function TestingComponent({ effectFn }: TestComponentProps) {
+    const [count, setCount] = useState<number>(0);
+    useDidUpdate(effectFn, [count]);
+
+    const onClickCountUp = () => {
+      setCount(count + 1);
+    };
+
+    return (
+      <button type="button" onClick={onClickCountUp}>
+        {TOSS_TEXT}
+      </button>
+    );
+  }
+  it('should not effect when mount', () => {
+    const effectFn = jest.fn();
+
+    expect(effectFn).not.toHaveBeenCalled();
+
+    render(<TestingComponent effectFn={effectFn} />);
+    expect(effectFn).not.toHaveBeenCalled();
+  });
+
+  it('should effect with dependency changes', () => {
+    const effectFn = jest.fn();
+
+    expect(effectFn).not.toHaveBeenCalled();
+
+    render(<TestingComponent effectFn={effectFn} />);
+    const button = screen.getByText(TOSS_TEXT);
+
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+
+    expect(effectFn).toHaveBeenCalled();
+
+    expect(effectFn).toHaveBeenCalledTimes(3);
+  });
+});
