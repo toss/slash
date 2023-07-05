3e78b7b9d64edfc6d47100ff7029ae4cd07ce5b3
wooki-kim <pcmwooki@gmail.com>
2023-07-05 19:01:05 +0900

    feat:(@toss/utils): Add duplicated values return to array keys in reverseKeyValue

--- packages/common/utils/src/reverseKeyValue.spec.ts
+++ packages/common/utils/src/reverseKeyValue.spec.ts
@@ -4,4 +4,11 @@ describe('reverseKeyValue 함수는', () => {
   it('key와 value를 바꿔 { [value]: key } 형태의 object를 반환한다.', () => {
     expect(reverseKeyValue({ jbee: 'eebj' })).toEqual({ eebj: 'jbee' });
   });
+
+  it('value가 중복되었을때  { [value]: [...key] } 형태의 object를 반환한다.', () => {
+    expect(reverseKeyValue({ ricky2: 'kim', jbee: 'eebj', jbee2: 'eebj', ricky: 'kim', denny: 'kim' })).toEqual({
+      eebj: ['jbee', 'jbee2'],
+      kim: ['ricky2', 'ricky', 'denny'],
+    });
+  });
 });
--- packages/common/utils/src/reverseKeyValue.ts
+++ packages/common/utils/src/reverseKeyValue.ts
@@ -1,4 +1,14 @@
 /** @tossdocs-ignore */
+
 export function reverseKeyValue<KeyType extends string, ValueType extends string>(obj: Record<KeyType, ValueType>) {
-  return Object.fromEntries(Object.entries(obj).map(([key, value]) => [value, key])) as Record<ValueType, KeyType>;
+  const reversedObj: Record<ValueType, KeyType | KeyType[]> = {} as Record<ValueType, KeyType | KeyType[]>;
+  for (const [key, value] of Object.entries(obj) as Array<[KeyType, ValueType]>) {
+    if (value in reversedObj) {
+      const existingValue = reversedObj[value];
+      reversedObj[value] = Array.isArray(existingValue) ? [...existingValue, key] : [existingValue, key];
+    } else {
+      reversedObj[value] = key;
+    }
+  }
+  return reversedObj;
 }
