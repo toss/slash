cd871fa7a779a70de3124ea1510693a1e4ff01d4
kim-elijah-sol <kimelijahsol@gmail.com>
2023-07-06 16:41:40 +0900

    fix(@toss/utils) : remove default parameter in isSubset

--- packages/common/utils/src/array/isSubset.ts
+++ packages/common/utils/src/array/isSubset.ts
@@ -1,5 +1,5 @@
 /** @tossdocs-ignore */
 
-export function isSubset(a: unknown[] = [], b: unknown[] = []): boolean {
+export function isSubset(a: unknown[], b: unknown[]): boolean {
   return a.every(element => b.includes(element));
 }
