86470b6cd7bce871081cc1e65d284dcd178dbabc
kim-elijah-sol <kimelijahsol@gmail.com>
2023-07-06 16:58:28 +0900

    fix(@toss/utils): write JSDoc for isSubset

--- packages/common/utils/src/array/isSubset.ts
+++ packages/common/utils/src/array/isSubset.ts
@@ -1,5 +1,20 @@
 /** @tossdocs-ignore */
 
+/**
+ * @name isSubset
+ * @description
+ * Checks if the first array is a subset of the second array
+ *
+ * @param subSet The array to check if it is a subset of the `wholeSet`
+ * @param wholeSet A set of comparison for `subSet`
+ *
+ * @returns boolean ( `subSet` is a subset of `wholeSet` )
+ *
+ * @example
+ * isSubset([], [1,2,3]); // true
+ * isSubset([1,2], [1,2,3]); // true
+ * isSubset([1,2,4], [1,2,3]); // false
+ */
 export function isSubset(subSet: unknown[], wholeSet: unknown[]): boolean {
   return subSet.every(element => wholeSet.includes(element));
 }
