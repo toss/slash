5c1b5f44a6035073de21387ce643b24f7891c72b
ChanhyukPark-Tech <pgg6713@gmail.com>
2023-07-22 22:49:34 +0900

    feat: Modify useLoading to avoid state updates when the component is unmounted

--- packages/react/use-loading/src/useLoading.ts
+++ packages/react/use-loading/src/useLoading.ts
@@ -1,16 +1,37 @@
 /** @tossdocs-ignore */
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 export function useLoading(): [boolean, <T>(promise: Promise<T>) => Promise<T>] {
   const [loading, setLoading] = useState(false);
-  const startTransition = useCallback(async <T>(promise: Promise<T>) => {
-    try {
-      setLoading(true);
-      const data = await promise;
-      return data;
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+  const ref = useIsMountedRef();
+
+  const startTransition = useCallback(
+    async <T>(promise: Promise<T>) => {
+      try {
+        setLoading(true);
+        const data = await promise;
+        return data;
+      } finally {
+        if (ref.isMounted) {
+          setLoading(false);
+        }
+      }
+    },
+    [ref.isMounted]
+  );
+
   return useMemo(() => [loading, startTransition], [loading, startTransition]);
+}
+
+function useIsMountedRef() {
+  const ref = useRef({ isMounted: true }).current;
+
+  useEffect(() => {
+    ref.isMounted = true;
+    return () => {
+      ref.isMounted = false;
+    };
+  }, [ref]);
+
+  return ref;
 }
