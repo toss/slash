904d766ff8758392d7726cba90b4b7c09ebb41a4
minsoo-web <zlemzlem5656@naver.com>
2023-07-23 19:59:20 +0900

    chore: Update test description ko to en

--- packages/react/use-loading/src/useLoading.spec.ts
+++ packages/react/use-loading/src/useLoading.spec.ts
@@ -1,26 +1,26 @@
-import { renderHook, act } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { useLoading } from './useLoading';
 
 describe('useLoading', () => {
   describe('() => Promise<Data>', () => {
     const mockedAPI = async () => ({ result: 'SUCCESS' });
-    it('startTransition 호출 전 loading: false', () => {
+    it('should false, when startTransition is not called', () => {
       const { result } = renderHook(() => useLoading());
       expect(result.current[0]).toBe(false);
     });
-    it('startTransition 호출 후 loading: true', async () => {
+    it('should true, when startTransition is called', async () => {
       const { result } = renderHook(() => useLoading());
       let promise: ReturnType<typeof mockedAPI>;
       act(() => {
         promise = result.current[1](mockedAPI());
       });
       expect(result.current[0]).toBe(true);
-      // hook 안에서 발생할 수 있는 mutation은 무조건 act 안에서 종료되어야 함
+      // Mutation that may occur in the hook must be terminated in the act unconditionally
       await act(async () => {
         await promise;
       });
     });
-    it('startTransition 완료 후 loading: false', async () => {
+    it(`should false, when startTransition's promise is resolved`, async () => {
       const { result } = renderHook(() => useLoading());
       await act(async () => {
         await result.current[1](mockedAPI());
@@ -29,7 +29,7 @@ describe('useLoading', () => {
         expect(result.current[0]).toBe(false);
       });
     });
-    it('startTransition 실패 시 loading: false', async () => {
+    it(`should false, when startTransition's promise is rejected`, async () => {
       const failingAPI = () => Promise.reject(new Error('sample error'));
       const { result } = renderHook(() => useLoading());
       await act(async () => {
@@ -42,11 +42,11 @@ describe('useLoading', () => {
   });
   describe('(arg: Arg) => Promise<Data>', () => {
     const mockedAPI = async (id: number) => ({ result: `id: ${id}` });
-    it('startTransition 호출 전 loading: false', () => {
+    it('should false, when startTransition is not called', () => {
       const { result } = renderHook(() => useLoading());
       expect(result.current[0]).toBe(false);
     });
-    it('startTransition 호출 후 loading: true', async () => {
+    it('should true, when startTransition is called', async () => {
       const { result } = renderHook(() => useLoading());
       let promise: ReturnType<typeof mockedAPI>;
       act(() => {
@@ -58,7 +58,7 @@ describe('useLoading', () => {
         await promise;
       });
     });
-    it('startTransition 완료 후 loading: false', async () => {
+    it(`should false, when startTransition's promise is resolved`, async () => {
       const { result } = renderHook(() => useLoading());
       await act(async () => {
         await result.current[1](mockedAPI(10));
@@ -67,7 +67,7 @@ describe('useLoading', () => {
         expect(result.current[0]).toBe(false);
       });
     });
-    it('startTransition 실패 시 loading: false', async () => {
+    it(`should false, when startTransition's promise is rejected`, async () => {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const failingAPI = (_: number) => Promise.reject(new Error('sample error'));
       const { result } = renderHook(() => useLoading());
@@ -80,7 +80,7 @@ describe('useLoading', () => {
     });
   });
   describe('(arg1: Arg1, arg2: Arg2) => Promise<Data> (type inference 테스트)', () => {
-    it('startTransition 완료 후 loading: false', async () => {
+    it(`should false, when startTransition's promise is resolved`, async () => {
       const mockedAPI = async (arg1: number, arg2: string) => ({ result: `${arg1}-${arg2}` });
       const { result } = renderHook(() => useLoading());
       await act(async () => {
