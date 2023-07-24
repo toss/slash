e4b738fab53a9e57c8eb584344585df1bfdd37a9
kim-elijah-sol <kimelijahsol@gmail.com>
2023-07-24 23:07:14 +0900

    test(@toss/utils): add case of comparing object types

--- packages/common/utils/src/array/isSubsest.spec.ts
+++ packages/common/utils/src/array/isSubsest.spec.ts
@@ -25,6 +25,18 @@ describe('isSubset', () => {
     expect(result).toBe(true);
   });
 
+  it('should return true if the first array of objects is a subset of the second array of objects', () => {
+    // Given
+    const value1 = [{ team: 'toss' }, { lib: 'utils' }];
+    const value2 = [{ team: 'toss' }, { language: 'typescript' }, { lib: 'utils' }];
+
+    // When
+    const result = isSubset(value1, value2);
+
+    // Then
+    expect(result).toBe(true);
+  });
+
   it('should return false if the first array is not a subset of the second array', () => {
     // Given
     const value1 = [1, 2, 4];
