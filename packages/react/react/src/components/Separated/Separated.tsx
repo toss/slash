a33849c7273302aabf033735da0a05e580dc1218
changyoung <changyoung@toss.im>
2023-07-25 17:41:27 +0900

    remove jsdoc

--- packages/react/react/src/components/Separated/Separated.tsx
+++ packages/react/react/src/components/Separated/Separated.tsx
@@ -5,25 +5,6 @@ type Props = {
   with: ReactNode;
 } & PropsWithChildren;
 
-/**
- * @name Separated
- * @description
- * List와 ListRow 아이템이 아니지만 요소들 중간에 반복적으로 들어가야 하는 컴포넌트가 있을 시 사용합니다.
- *
- * ```typescript
- * <Separated
- *   // 요소들 중간에 들어갈 컴포넌트 (`ReactNode`)
- *   with={with}
- * >
- *   {children}
- * </Separated>
- * ```
- *
- * @example
- * <Separated with={<Border type="padding24" />}>
- *   {LIST.map(item => <div>item.title</div>)}
- * </Separated>
- */
 export function Separated({ children, with: separator }: Props) {
   const childrenArray = Children.toArray(children).filter(isValidElement);
   const childrenLength = childrenArray.length;
--- packages/react/react/src/utils/buildContext.tsx
+++ packages/react/react/src/utils/buildContext.tsx
@@ -2,28 +2,6 @@ import { createContext, ReactNode, useContext, useMemo } from 'react';
 
 type ProviderProps<ContextValuesType> = (ContextValuesType & { children: ReactNode }) | { children: ReactNode };
 
-/**
- * @description React Context를 정의할 때 반복되는 코드를 줄여주는 헬퍼 함수입니다.
- * @param contextName 기능을 드러내는 이름이면 좋습니다. context의 정체성을 담당합니다.
- * @param defaultContextValues 기본값을 지정할 수 있습니다.
- * @returns [Provider, useContext]를 반환합니다.
- * @example
- * const [Provider, useContext] = buildContext('TestContext', null);
- *
- * function Inner() {
- *  const context = useContext<{ title: string }>('Inner');
- *
- * return <h1>{context.title}</h1>;
- * }
- *
- * function Page() {
- *  return (
- *    <Provider title="타이틀">
- *      <Inner />
- *    </Provider>
- *  );
- * }
- */
 export function buildContext<ContextValuesType extends object>(
   contextName: string,
   defaultContextValues?: ContextValuesType | null
