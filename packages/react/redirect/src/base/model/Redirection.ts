cab1281c552452ec681fafa6cc6d8d9b87f65c5e
Sung Jeon <basix@basix.tech>
2023-07-25 21:03:09 +0900

    feat(redirection): Merge internal changes (#309)
    * merge(redirection): merge internal changes
    
    * fix import
    
    * Update packages/react/redirect/src/base/boundary/RedirectionBoundary.tsx
    
    ---------
    
    Co-authored-by: Sojin Park <raon0211@toss.im>

--- packages/react/redirect/src/base/model/Redirection.ts
+++ packages/react/redirect/src/base/model/Redirection.ts
@@ -1,11 +1,9 @@
-/** @tossdocs-ignore */
 export interface BaseRedirectionOptions {
   destination: string;
+  external?: boolean;
 }
 
-export type RedirectionOptions<T extends BaseRedirectionOptions = BaseRedirectionOptions> = {
-  onRedirect?: (options: T) => void;
-} & T;
+export type RedirectionOptions<T extends BaseRedirectionOptions = BaseRedirectionOptions> = T;
 
 export class Redirection<T extends BaseRedirectionOptions = BaseRedirectionOptions> extends Error {
   readonly name = 'Redirection';
@@ -18,16 +16,15 @@ export class Redirection<T extends BaseRedirectionOptions = BaseRedirectionOptio
     return new this(options);
   }
 
+  static isRedirection<T extends BaseRedirectionOptions = BaseRedirectionOptions>(
+    data: unknown
+  ): data is Redirection<T> {
+    return data != null && (data as any)?.name === 'Redirection';
+  }
+
   constructor(options: RedirectionOptions<T>) {
     super(`Redirection to ${options.destination}`);
     this.options = options;
-  }
-
-  async redirect() {
-    if (this.options.onRedirect == null) {
-      throw new Error('Redirection method not defined');
-    }
-    this.options.onRedirect?.(this.options);
   }
 }
 
