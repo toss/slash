f5584a09f6e757782511ce2854a4dfb033f51dae
ssi02014 <ssi02014@naver.com>
2023-07-30 02:44:09 +0900

    test(@toss/utils): Add getViewportSize Test Code

--- packages/common/utils/src/getViewportSize.spec.tsx
+++ packages/common/utils/src/getViewportSize.spec.tsx
@@ -5,6 +5,31 @@ const setViewportSize = (width: number, height: number) => {
   Object.defineProperty(window, 'innerHeight', { writable: true, configurable: true, value: height });
 };
 
+const deleteWindow = () => {
+  Object.defineProperty(global, 'window', {
+    value: undefined,
+  });
+};
+
+const initViewportSize = () => {
+  // jest sets innerWidth 1024 and innerHeight 768 as the default values for the jsdom to be tested.
+  // https://github.com/jsdom/jsdom/blob/0cba358253fd5530af0685ac48c2535992464d06/lib/jsdom/browser/Window.js#L587-L588
+  setViewportSize(1024, 768);
+};
+
+beforeEach(() => {
+  initViewportSize();
+});
+
+describe('setViewportSize', () => {
+  it('should change the innerWidth to 100 and the innerHeight to 200', () => {
+    setViewportSize(100, 200);
+
+    expect(window.innerWidth).toBe(100);
+    expect(window.innerHeight).toBe(200);
+  });
+});
+
 describe('getViewportSize', () => {
   it("Jest's viewport default settings should return width 1024 and height 768", () => {
     const { width, height } = getViewportSize();
@@ -26,5 +51,16 @@ describe('getViewportSize', () => {
 
     expect(width).toBe(500);
     expect(height).toBe(300);
+  });
+
+  it('should return width 0 and height 0 if window is undefined', () => {
+    deleteWindow();
+
+    const { width, height } = getViewportSize();
+
+    expect(window).toBeUndefined();
+
+    expect(width).toBe(0);
+    expect(height).toBe(0);
   });
 });
