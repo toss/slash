7418544426fd572e62fc18ef5447e10ad3cef510
SEOKKAMONI <sj01048260279@gmail.com>
2023-10-09 02:17:48 +0900

    chore(@toss/use-overlay): fix null exception

--- packages/react/use-overlay/src/useOverlay.tsx
+++ packages/react/use-overlay/src/useOverlay.tsx
@@ -13,7 +13,7 @@ interface Options {
 export function useOverlay({ exitOnUnmount = true }: Options = {}) {
   const context = useContext(OverlayContext);
 
-  if (context == null) {
+  if (!context) {
     throw new Error('useOverlay is only available within OverlayProvider.');
   }
 
