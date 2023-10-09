cd9ab9a7c75e2353c768ae1beda3f6012ba9870b
SEOKKAMONI <sj01048260279@gmail.com>
2023-10-10 01:06:35 +0900

    chore(@toss/react): rewrite test code in english

--- packages/react/react/src/utils/convertNewlineToJSX.test.tsx
+++ packages/react/react/src/utils/convertNewlineToJSX.test.tsx
@@ -3,13 +3,13 @@ import { create } from 'react-test-renderer';
 import { convertNewlineToJSX } from './convertNewlineToJSX';
 
 describe('convertNewlineToJSX', () => {
-  it('개행 문자를 JSX 요소로 변환해야 합니다', () => {
-    const input = '로그인 시도 중 문제가 발생했습니다.\n다시 시도해 주세요.';
+  it('converts newline characters into <br /> JSX elements', () => {
+    const input = 'There was a problem trying to sign in.\nplease try again.';
     const output = [
-      <React.Fragment key={0}>로그인 시도 중 문제가 발생했습니다.</React.Fragment>,
+      <React.Fragment key={0}>There was a problem trying to sign in.</React.Fragment>,
       <React.Fragment key={1}>
         <br />
-        다시 시도해 주세요.
+        please try again.
       </React.Fragment>,
     ];
 
