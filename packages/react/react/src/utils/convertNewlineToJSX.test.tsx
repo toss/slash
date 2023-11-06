0ba124b6fe95f6a6cb197e39a17b5ec73c1d2d1f
김석진 <102217654+SEOKKAMONI@users.noreply.github.com>
2023-11-06 16:06:21 +0900

    Update packages/react/react/src/utils/convertNewlineToJSX.test.tsx
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>

--- packages/react/react/src/utils/convertNewlineToJSX.test.tsx
+++ packages/react/react/src/utils/convertNewlineToJSX.test.tsx
@@ -3,7 +3,7 @@ import { create } from 'react-test-renderer';
 import { convertNewlineToJSX } from './convertNewlineToJSX';
 
 describe('convertNewlineToJSX', () => {
-  it('converts newline characters into <br /> JSX elements', () => {
+  it('should converts newline characters into <br /> JSX elements', () => {
     const input = 'There was a problem trying to sign in.\nplease try again.';
     const output = [
       <React.Fragment key={0}>There was a problem trying to sign in.</React.Fragment>,
