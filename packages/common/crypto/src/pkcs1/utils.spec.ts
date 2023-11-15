56939f3cff36725eab0df1c9f77237e57ceedaec
po4tion <po4tion0429@gmail.com>
2023-11-15 19:03:28 +0900

    fix: test method to it method

--- packages/common/crypto/src/pkcs1/utils.spec.ts
+++ packages/common/crypto/src/pkcs1/utils.spec.ts
@@ -2,28 +2,28 @@ import BigNumber from 'bn.js';
 import { powerMod } from './utils';
 
 describe('powerMod function tests', () => {
-  test('powerMod with small numbers', () => {
+  it('powerMod with small numbers', () => {
     const n = new BigNumber(2);
     const p = new BigNumber(3);
     const m = new BigNumber(5);
     expect(powerMod(n, p, m).toString()).toBe(new BigNumber(3).toString()); // 2^3 % 5 = 8 % 5 = 3
   });
 
-  test('powerMod with p = 1 should return n', () => {
+  it('powerMod with p = 1 should return n', () => {
     const n = new BigNumber(7);
     const p = new BigNumber(1);
     const m = new BigNumber(10);
     expect(powerMod(n, p, m).toString()).toBe(n.toString()); // 7^1 % 10 = 7
   });
 
-  test('powerMod with even p', () => {
+  it('powerMod with even p', () => {
     const n = new BigNumber(4);
     const p = new BigNumber(4);
     const m = new BigNumber(17);
     expect(powerMod(n, p, m).toString()).toBe(new BigNumber(1).toString()); // 4^4 % 17 = 256 % 17 = 1
   });
 
-  test('powerMod with odd p', () => {
+  it('powerMod with odd p', () => {
     const n = new BigNumber(5);
     const p = new BigNumber(3);
     const m = new BigNumber(13);
