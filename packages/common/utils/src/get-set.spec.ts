ec8ae27c261589c41a24e4da9d0b7a00e1c2a6f1
정원 <66169832+JayeHa@users.noreply.github.com>
2023-11-16 18:11:21 +0900

    chore: translate get-set test descriptions from Korean to English

--- packages/common/utils/src/get-set.spec.ts
+++ packages/common/utils/src/get-set.spec.ts
@@ -1,26 +1,30 @@
 import { get, set } from './get-set';
 
-describe('get은', () => {
-  it('한 단계 path를 잘 get한다', () => {
+describe('get function', () => {
+  it('correctly retrieves a value for a single-level property', () => {
     expect(get({ a: { b: { c: 'd' } } }, 'a')).toStrictEqual({ b: { c: 'd' } });
   });
-  it('여러 단계 path를 잘 get한다', () => {
+
+  it('correctly retrieves a value for a multi-level property', () => {
     expect(get({ a: { b: { c: 'd' } } }, 'a.b')).toStrictEqual({ c: 'd' });
     expect(get({ a: { b: { c: 'd' } } }, 'a.b.c')).toStrictEqual('d');
   });
-  it('경로에 해당하는 값이 없을 때 기본값을 반환한다', () => {
-    expect(get({ a: { b: { c: 'd' } } }, 'a.c',"f")).toStrictEqual("f");
+
+  it('returns a default value when the specified path does not exist', () => {
+    expect(get({ a: { b: { c: 'd' } } }, 'a.c', 'default')).toStrictEqual('default');
   });
 });
 
-describe('set은', () => {
-  it('한 단계 path를 잘 set한다', () => {
+describe('set function', () => {
+  it('correctly sets a value for a single-level property', () => {
     expect(set({ a: { b: { c: 'd' } } }, 'a', 'e')).toStrictEqual({ a: 'e' });
   });
-  it('여러 단계 path를 잘 set한다', () => {
+
+  it('correctly sets a value for a multi-level property', () => {
     expect(set({ a: { b: { c: 'd' } } }, 'a.b.c', 'e')).toStrictEqual({ a: { b: { c: 'e' } } });
   });
-  it('여러 단계 path를 잘 set한다', () => {
+
+  it('correctly adds a new property at a multi-level path', () => {
     expect(set({ a: { b: { c: 'c' } } }, 'a.b.e', 'e')).toStrictEqual({ a: { b: { c: 'c', e: 'e' } } });
   });
 });
