794d0c2737ec37120b479fc0b9408a8426243585
정원 <66169832+JayeHa@users.noreply.github.com>
2023-11-16 18:32:45 +0900

    chore: update test description

--- packages/common/utils/src/get-set.spec.ts
+++ packages/common/utils/src/get-set.spec.ts
@@ -1,39 +1,39 @@
 import { get, set } from './get-set';
 
 describe('get function', () => {
-  it('correctly retrieves a value for a single-level property', () => {
+  it('should correctly retrieve a value for a single-level property', () => {
     expect(get({ a: { b: { c: 'd' } } }, 'a')).toStrictEqual({ b: { c: 'd' } });
   });
-  it('correctly retrieves a value for a multi-level property', () => {
+  it('should correctly retrieve a value for a multi-level property', () => {
     expect(get({ a: { b: { c: 'd' } } }, 'a.b')).toStrictEqual({ c: 'd' });
     expect(get({ a: { b: { c: 'd' } } }, 'a.b.c')).toStrictEqual('d');
   });
-  it('returns a default value when the specified path does not exist', () => {
+  it('should return a default value when the specified path does not exist', () => {
     expect(get({ a: { b: { c: 'd' } } }, 'a.c', 'default')).toStrictEqual('default');
   });
-  it('ignores consecutive dots in path and retrieves the correct value', () => {
+  it('should ignore consecutive dots in path and retrieve the correct value', () => {
     expect(get({ a: { b: { c: 'd' } } }, 'a..b')).toStrictEqual({ c: 'd' });
   });
-  it('works correctly with empty objects', () => {
+  it('should work correctly with empty objects', () => {
     expect(get({}, 'a')).toBeUndefined();
   });
 });
 
 describe('set function', () => {
-  it('correctly sets a value for a single-level property', () => {
+  it('should correctly set a value for a single-level property', () => {
     expect(set({ a: { b: { c: 'd' } } }, 'a', 'e')).toStrictEqual({ a: 'e' });
   });
-  it('correctly sets a value for a multi-level property', () => {
+  it('should correctly set a value for a multi-level property', () => {
     expect(set({ a: { b: { c: 'd' } } }, 'a.b.c', 'e')).toStrictEqual({ a: { b: { c: 'e' } } });
   });
-  it('correctly adds a new property at a multi-level path', () => {
+  it('should correctly add a new property at a multi-level path', () => {
     expect(set({ a: { b: { c: 'c' } } }, 'a.b.e', 'e')).toStrictEqual({ a: { b: { c: 'c', e: 'e' } } });
   });
-  it('assuming the function does not modify the object in case of an incorrect path', () => {
+  it('should handle incorrect path formats gracefully', () => {
     const obj = { a: { b: { c: 'd' } } };
     expect(set(obj, 'a..b', 'e')).toStrictEqual(obj);
   });
-  it('works correctly with empty objects', () => {
+  it('should work correctly with empty objects', () => {
     expect(set({}, 'a', 'b')).toStrictEqual({ a: 'b' });
   });
 });
