d011043d7d9f586a20dc35942058eaa22a9bb817
kim-elijah-sol <kimelijahsol@gmail.com>
2023-11-25 22:00:29 +0900

    fix(@toss/utils): rename subSet to subset in isSubset function

--- packages/common/utils/src/array/isSubset.ts
+++ packages/common/utils/src/array/isSubset.ts
@@ -6,10 +6,10 @@ import deepEqual from 'fast-deep-equal';
  * @description
  * Checks if the first array is a subset of the second array
  *
- * @param subSet The array to check if it is a subset of the `wholeSet`
- * @param wholeSet A set of comparison for `subSet`
+ * @param subset The array to check if it is a subset of the `wholeSet`
+ * @param wholeSet A set of comparison for `subset`
  *
- * @returns boolean ( `subSet` is a subset of `wholeSet` )
+ * @returns boolean ( `subset` is a subset of `wholeSet` )
  *
  * @example
  * isSubset([], [1,2,3]); // true
@@ -17,6 +17,6 @@ import deepEqual from 'fast-deep-equal';
  * isSubset([{a: 1}, {c: 3}], [{a: 1}, {b: 2}, {c: 3}]) // true
  * isSubset([1,2,4], [1,2,3]); // false
  */
-export function isSubset(subSet: unknown[], wholeSet: unknown[]): boolean {
-  return subSet.every(element => wholeSet.some(wholeSetElement => deepEqual(wholeSetElement, element)));
+export function isSubset(subset: unknown[], wholeSet: unknown[]): boolean {
+  return subset.every(element => wholeSet.some(wholeSetElement => deepEqual(wholeSetElement, element)));
 }
