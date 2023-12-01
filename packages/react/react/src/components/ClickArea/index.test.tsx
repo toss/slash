dfead6b4746e5eca1ab7ff3225600db91c0bfcba
정원 <66169832+JayeHa@users.noreply.github.com>
2023-12-02 03:00:09 +0900

    test: translate test description

--- packages/react/react/src/components/ClickArea/index.test.tsx
+++ packages/react/react/src/components/ClickArea/index.test.tsx
@@ -2,24 +2,24 @@ import { render } from '@testing-library/react';
 import { ClickArea } from '.';
 
 describe('ClickArea', () => {
-  it('정상적으로 rendering 된다', () => {
+  it('should render correctly', () => {
     const result = render(<ClickArea enabled={true}>click</ClickArea>);
 
     expect(result.getByText('click')).toBeInTheDocument();
   });
-  it('enabled 가 true일 때 classname에 tossteam-react__click-area__enabled가 포함된다', () => {
+  it('should apply enabled class when enabled', () => {
     const result = render(<ClickArea enabled={true}>click</ClickArea>);
 
     expect(result.getAllByRole('button')[0]).toHaveClass('tossteam-react__click-area__enabled');
   });
 
-  it('enabled 가 false 때 classname에 tossteam-react__click-area__enabled가 포함되지 않는다', () => {
+  it('should not apply enabled class when not enabled', () => {
     const result = render(<ClickArea enabled={false}>click</ClickArea>);
 
     expect(result.getAllByRole('button')[0]).not.toHaveClass('tossteam-react__click-area__enabled');
   });
 
-  it('enabled 가 true일 때 onClick 이벤트가 발생한다', () => {
+  it('should call onClick when enabled and clicked', () => {
     const onClick = jest.fn();
     const result = render(
       <ClickArea enabled={true} onClick={onClick}>
@@ -32,7 +32,7 @@ describe('ClickArea', () => {
     expect(onClick).toHaveBeenCalled();
   });
 
-  it('enabled 가 false일 때 onClick 이벤트가 발생하지 않는다', () => {
+  it('should not call onClick when not enabled and clicked', () => {
     const onClick = jest.fn();
     const result = render(
       <ClickArea enabled={false} onClick={onClick}>
