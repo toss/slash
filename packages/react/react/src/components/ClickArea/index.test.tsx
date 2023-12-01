96b32acbb1f27f1cc0be6e1de5bae7a6a4f2a49b
정원 <66169832+JayeHa@users.noreply.github.com>
2023-12-02 03:06:36 +0900

    test: update test of userEvent

--- packages/react/react/src/components/ClickArea/index.test.tsx
+++ packages/react/react/src/components/ClickArea/index.test.tsx
@@ -1,38 +1,36 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { ClickArea } from '.';
 
 describe('ClickArea', () => {
+  const ENABLED_CLASS_NAME = 'tossteam-react__click-area__enabled';
+
   it('should render correctly', () => {
-    const result = render(<ClickArea enabled={true}>click</ClickArea>);
-
-    expect(result.getByText('click')).toBeInTheDocument();
+    const result = render(<ClickArea>click</ClickArea>);
+    expect(result.getByRole('button')).toHaveTextContent('click');
   });
-  it('should apply enabled class when enabled', () => {
-    const result = render(<ClickArea enabled={true}>click</ClickArea>);
 
-    expect(result.getAllByRole('button')[0]).toHaveClass('tossteam-react__click-area__enabled');
+  it('should apply enabled class when enabled', () => {
+    const result = render(<ClickArea enabled>click</ClickArea>);
+    expect(result.getByRole('button')).toHaveClass(ENABLED_CLASS_NAME);
   });
 
   it('should not apply enabled class when not enabled', () => {
     const result = render(<ClickArea enabled={false}>click</ClickArea>);
-
-    expect(result.getAllByRole('button')[0]).not.toHaveClass('tossteam-react__click-area__enabled');
+    expect(result.getByRole('button')).not.toHaveClass(ENABLED_CLASS_NAME);
   });
 
-  it('should call onClick when enabled and clicked', () => {
+  it('should call onClick when enabled and clicked', async () => {
+    const user = userEvent.setup();
     const onClick = jest.fn();
-    const result = render(
-      <ClickArea enabled={true} onClick={onClick}>
-        click
-      </ClickArea>
-    );
+    const result = render(<ClickArea onClick={onClick}>click</ClickArea>);
 
-    result.getAllByRole('button')[0].click();
-
-    expect(onClick).toHaveBeenCalled();
+    await user.click(result.getByRole('button'));
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 
-  it('should not call onClick when not enabled and clicked', () => {
+  it('should not call onClick when not enabled and clicked', async () => {
+    const user = userEvent.setup();
     const onClick = jest.fn();
     const result = render(
       <ClickArea enabled={false} onClick={onClick}>
@@ -40,8 +38,7 @@ describe('ClickArea', () => {
       </ClickArea>
     );
 
-    result.getAllByRole('button')[0].click();
-
+    await user.click(result.getByRole('button'));
     expect(onClick).not.toHaveBeenCalled();
   });
 });
