d2eb69a718cd629ced53b8f82081cdb06e563879
정원 <jwha94@gmail.com>
2023-12-04 12:54:08 +0900

    test(@toss/utils): Add test code for `uniqWith` (#367)
    * test(utils): add test code for uniqWith
    
    * chore: rename 'arr' to 'inputList' and introduce 'expectedList' for clarify

---
+++ packages/common/utils/src/uniqWith.spec.ts
@@ -0,0 +1,29 @@
+import { uniqWith } from './uniqWith';
+
+describe('uniqWith', () => {
+  it('should remove duplicates from an array based on comparator', () => {
+    const inputList = [
+      { x: 1, y: 2 },
+      { x: 2, y: 1 },
+      { x: 1, y: 2 },
+    ];
+    const expectedList = [
+      { x: 1, y: 2 },
+      { x: 2, y: 1 },
+    ];
+
+    expect(uniqWith(inputList, (x, y) => x.x === y.x && x.y === y.y)).toEqual(expectedList);
+  });
+
+  it('should return same array when no duplicates in array', () => {
+    expect(uniqWith([1, 2, 3, 4, 5], (x, y) => x === y)).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('should return correctly when all values duplicates in array', () => {
+    expect(uniqWith([1, 1, 1, 1, 1], (x, y) => x === y)).toEqual([1]);
+  });
+
+  it('should return an empty array when given an empty array', () => {
+    expect(uniqWith([], (x, y) => x === y)).toEqual([]);
+  });
+});
