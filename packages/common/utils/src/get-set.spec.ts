970bc9ff931f34fbfb82c95661634c9b2d68c582
Dongkyu Kim <po4tion0429@gmail.com>
2023-12-16 19:50:31 +0900

    test(@toss/utils): Improve tests for get-set (#395)
    * test: improve test code
    
    * fix: remove useless test code
    
    * test: separate get function test case

--- packages/common/utils/src/get-set.spec.ts
+++ packages/common/utils/src/get-set.spec.ts
@@ -1,21 +1,32 @@
 import { get, set } from './get-set';
 
 describe('get function', () => {
-  it('should correctly retrieve a value for a single-level property', () => {
-    expect(get({ a: { b: { c: 'd' } } }, 'a')).toStrictEqual({ b: { c: 'd' } });
+  describe('Returns a valid value.', () => {
+    it('should correctly retrieve a value for a single-level property', () => {
+      expect(get({ a: { b: { c: 'd' } } }, 'a')).toStrictEqual({ b: { c: 'd' } });
+    });
+    it('should correctly retrieve a value for a multi-level property', () => {
+      expect(get({ a: { b: { c: 'd' } } }, 'a.b')).toStrictEqual({ c: 'd' });
+      expect(get({ a: { b: { c: 'd' } } }, 'a.b.c')).toStrictEqual('d');
+    });
+    it('should return a default value when the specified path does not exist', () => {
+      expect(get({ a: { b: { c: 'd' } } }, 'a.c', 'default')).toStrictEqual('default');
+    });
+    it('should ignore consecutive dots in path and retrieve the correct value', () => {
+      expect(get({ a: { b: { c: 'd' } } }, 'a..b')).toStrictEqual({ c: 'd' });
+    });
   });
-  it('should correctly retrieve a value for a multi-level property', () => {
-    expect(get({ a: { b: { c: 'd' } } }, 'a.b')).toStrictEqual({ c: 'd' });
-    expect(get({ a: { b: { c: 'd' } } }, 'a.b.c')).toStrictEqual('d');
-  });
-  it('should return a default value when the specified path does not exist', () => {
-    expect(get({ a: { b: { c: 'd' } } }, 'a.c', 'default')).toStrictEqual('default');
-  });
-  it('should ignore consecutive dots in path and retrieve the correct value', () => {
-    expect(get({ a: { b: { c: 'd' } } }, 'a..b')).toStrictEqual({ c: 'd' });
-  });
-  it('should work correctly with empty objects', () => {
-    expect(get({}, 'a')).toBeUndefined();
+
+  describe('Returns null or undefined', () => {
+    it('should work correctly with empty objects', () => {
+      expect(get({}, 'a')).toBeUndefined();
+    });
+    it('should handle paths leading to undefined or null values inside nested objects', () => {
+      const obj = { a: { b: undefined, c: null } };
+      expect(get(obj, 'a.b')).toBeUndefined();
+      expect(get(obj, 'a.c')).toBeNull();
+      expect(get(obj, 'a.b.d')).toBeUndefined();
+    });
   });
 });
 
