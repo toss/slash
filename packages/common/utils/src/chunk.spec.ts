c92d4879c3e8d994fc14e08dc94ddbde2f04d907
Dongkyu Kim <po4tion0429@gmail.com>
2023-12-19 15:22:34 +0900

    test(@toss/utils): Translate Korean to English (#393)

--- packages/common/utils/src/chunk.spec.ts
+++ packages/common/utils/src/chunk.spec.ts
@@ -1,23 +1,23 @@
 import { chunk } from './chunk';
 
-describe('chunk는', () => {
-  it('빈배열 입력시 빈배열 리턴한다', () => {
+describe('chunk', () => {
+  it('should return an empty array when the input array is empty', () => {
     expect(chunk([], 3)).toEqual([]);
   });
 
-  it('size가 1 미만인 경우 빈배열을 리턴한다.', () => {
+  it('should return an empty array when the size is less than 1', () => {
     expect(chunk([1, 2, 3], 0)).toEqual([]);
     expect(chunk([1, 2, 3], -1)).toEqual([]);
   });
 
-  it('전체 길이가 size의 배수인 경우 모든 엘리먼트가 사이즈만큼 고르게 나눠진다', () => {
+  it('should evenly divide all elements into chunks of the specified size when the total length is a multiple of the size', () => {
     expect(chunk([1, 2, 3, 4, 5, 6], 3)).toEqual([
       [1, 2, 3],
       [4, 5, 6],
     ]);
   });
 
-  it('전체 길이가 size의 배수가 아닐 때 마지막 청크는 남은 엘리먼트들이다.', () => {
+  it('should place the remaining elements in the last chunk when the total length is not a multiple of the size', () => {
     expect(chunk([1, 2, 3, 4], 6)).toEqual([[1, 2, 3, 4]]);
     expect(chunk([1, 2, 3, 4, 5, 6, 7], 2)).toEqual([[1, 2], [3, 4], [5, 6], [7]]);
   });
