970c0baa31a08e1d31b4d74c356a132278e0654b
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-26 05:27:04 +0900

    test: iterateEnum 테스트 코드 추가

---
+++ packages/common/utils/src/iterateEnum.spec.ts
@@ -0,0 +1,26 @@
+enum Language {
+  KOR = 'KOR',
+  ENG = 'ENG',
+  JPN = 'JPN',
+}
+
+export default describe('iterateEnum', () => {
+  it('should iterate over enum correctly', () => {
+    const expectedArray = [Language.KOR, Language.ENG, Language.JPN];
+
+    expect(iterateEnum(Language)).toEqual(expectedArray);
+  });
+
+  it('should produce the same result as Object.keys', () => {
+    const expectedArray = Object.keys(Language);
+
+    expect(iterateEnum(Language)).toEqual(expectedArray);
+  });
+
+  it('should infer the correct type', () => {
+    const result = iterateEnum(Language);
+    const typeCheck: Language[] = result;
+
+    expect(typeCheck).toBeTruthy();
+  });
+});
