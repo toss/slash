717cef643d827d594a96a6ef7d3429b928fd345e
김석진(maru) <102217654+SEOKKAMONI@users.noreply.github.com>
2024-01-31 22:08:06 +0900

    Update packages/common/utils/src/isNil.ts

--- packages/common/utils/src/isNil.ts
+++ packages/common/utils/src/isNil.ts
@@ -1,4 +1,4 @@
 /** @tossdocs-ignore */
-export function isNil<T>(val: T | undefined | null): val is T {
+export function isNil<T>(val: T | undefined | null): val is null | undefined {
   return val == null;
 }
