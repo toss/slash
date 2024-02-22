aacc3d37a40a90a1ef0f6dfa3a2a25cd6c54fbcf
Gaic4o <alstntorl@naver.com>
2024-02-22 23:59:32 +0900

    test(@toss/react): rewrite useInterval test code in english

--- packages/react/react/src/hooks/useInterval.spec.tsx
+++ packages/react/react/src/hooks/useInterval.spec.tsx
@@ -4,7 +4,7 @@ import { useInterval } from './useInterval';
 jest.useFakeTimers();
 
 describe('useInterval', () => {
-  it('정해진 시간이 지날때마다, 콜백을 실행한다', () => {
+  it('Should execute a callback every time a set time passes.', () => {
     const callback = jest.fn();
 
     renderHook(() =>
@@ -26,7 +26,7 @@ describe('useInterval', () => {
     expect(callback).toBeCalledTimes(2);
   });
 
-  it('number option도 동일하게실행한다', () => {
+  it('should work the same with number options as well.', () => {
     const callback = jest.fn();
 
     renderHook(() => useInterval(callback, 3000));
