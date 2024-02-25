9c3d86406611644ae7b0dc0b2ddd3ccabde1e29c
Saul Lee <solleedata@gmail.com>
2024-02-26 01:13:38 +0900

    test: Comment out tests and utils

--- packages/common/utils/src/device/isAndroid.spec.ts
+++ packages/common/utils/src/device/isAndroid.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it, jest } from '@jest/globals';
 import { isAndroid } from './isAndroid';
 
 describe('isAndroid', () => {
@@ -9,14 +10,14 @@ describe('isAndroid', () => {
     expect(isAndroid).toBe(false);
   });
 
-  // it('should return true for Android user agent', () => {
-  //   jest.mock('./isServer', () => ({
-  //     isServer: false,
-  //   }));
-  //   Object.defineProperty(window.navigator, 'userAgent', { value: 'Android', writable: true });
+  it('should return true for Android user agent', () => {
+    // jest.mock('./isServer', () => ({
+    //   isServer: false,
+    // }));
+    Object.defineProperty(window.navigator, 'userAgent', { value: 'Android', writable: true });
 
-  //   expect(isAndroid).toBe(true);
-  // });
+    expect(isAndroid).toBe(true);
+  });
 
   it('should return false for non-Android user agent', () => {
     jest.mock('./isServer', () => ({
--- packages/common/utils/src/device/isIOS.spec.ts
+++ packages/common/utils/src/device/isIOS.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it, jest } from '@jest/globals';
 import { isIOS } from './isIOS';
 
 describe('isIOS', () => {
@@ -9,14 +10,14 @@ describe('isIOS', () => {
     expect(isIOS).toBe(false);
   });
 
-  // it('should return true for iOS user agent', () => {
-  //   jest.mock('./isServer', () => ({
-  //     isServer: false,
-  //   }));
-  //   Object.defineProperty(window.navigator, 'userAgent', { value: 'iPhone', writable: true });
+  it('should return true for iOS user agent', () => {
+    // jest.mock('./isServer', () => ({
+    //   isServer: false,
+    // }));
+    Object.defineProperty(window.navigator, 'userAgent', { value: 'iPhone', writable: true });
 
-  //   expect(isIOS).toBe(true);
-  // });
+    expect(isIOS).toBe(true);
+  });
 
   it('should return false for non-iOS user agent', () => {
     jest.mock('./isServer', () => ({
