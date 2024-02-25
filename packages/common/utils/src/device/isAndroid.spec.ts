59e1202d609ddff5484d72ea39f11b2cb1f20ca1
Saul Lee <solleedata@gmail.com>
2024-02-25 19:28:59 +0900

    test(@toss/utils): Modify test code for isAndroid

--- packages/common/utils/src/device/isAndroid.spec.ts
+++ packages/common/utils/src/device/isAndroid.spec.ts
@@ -1,22 +1,27 @@
 import { isAndroid } from './isAndroid';
-import * as serverCheckModule from './isServer';
 
 describe('isAndroid', () => {
   it('should return false if on server', () => {
-    jest.spyOn(serverCheckModule, 'isServer').mockReturnValue(true);
+    jest.mock('./isServer', () => ({
+      isServer: () => true,
+    }));
 
     expect(isAndroid).toBe(false);
   });
 
   it('should return true for Android user agent', () => {
-    jest.spyOn(serverCheckModule, 'isServer').mockReturnValue(false);
+    jest.mock('./isServer', () => ({
+      isServer: () => false,
+    }));
     Object.defineProperty(window.navigator, 'userAgent', { value: 'Android', writable: true });
 
     expect(isAndroid).toBe(true);
   });
 
   it('should return false for non-Android user agent', () => {
-    jest.spyOn(serverCheckModule, 'isServer').mockReturnValue(false);
+    jest.mock('./isServer', () => ({
+      isServer: () => false,
+    }));
     Object.defineProperty(window.navigator, 'userAgent', { value: 'iPhone', writable: true });
 
     expect(isAndroid).toBe(false);
