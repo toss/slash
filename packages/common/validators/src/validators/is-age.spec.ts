c998a5921900679141ae5b64f63a9ec54c2d0e45
Dongkyu Kim <po4tion0429@gmail.com>
2024-03-22 17:26:54 +0900

    test(@toss/validators): translate into english (#419)

--- packages/common/validators/src/validators/is-age.spec.ts
+++ packages/common/validators/src/validators/is-age.spec.ts
@@ -1,7 +1,7 @@
 import { isAge } from './is-age';
 
 describe('isAge', () => {
-  test('자연수만 유효하다.', () => {
+  it('should only be valid for natural numbers.', () => {
     const validInputs = ['123', '5', '1', '88'];
     const invalidInputs = ['0', '5.2', '-0', 'NaN', 'undefined', 'null', '5e+23', 'Infinity'];
 
