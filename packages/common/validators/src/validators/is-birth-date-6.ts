6f2a44d02dd494c2a9320c5d44417cea1e6ff27a
po4tion <po4tion0429@gmail.com>
2024-03-26 21:45:24 +0900

    refactor(@toss/validators): use proper library(date-fns)

--- packages/common/validators/src/validators/is-birth-date-6.ts
+++ packages/common/validators/src/validators/is-birth-date-6.ts
@@ -1,30 +1,8 @@
 /** @tossdocs-ignore */
+import { isValid, parse } from 'date-fns';
+
 export function isBirthDate6(birthDate: string) {
-  if (!/^\d{6}$/.test(birthDate)) {
-    return false;
-  }
+  const parsed = parse(birthDate, 'yyMMdd', new Date());
 
-  const year = parseInt(birthDate.substring(0, 2), 10);
-  const month = parseInt(birthDate.substring(2, 4), 10);
-  const day = parseInt(birthDate.substring(4, 6), 10);
-
-  const JANUARY = 1;
-  const DECEMBER = 12;
-
-  if (month < JANUARY || month > DECEMBER) {
-    return false;
-  }
-
-  const daysInFebruary = isLeapYear(year) ? 29 : 28;
-  const daysInMonth = [31, daysInFebruary, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-  if (day < 1 || day > daysInMonth[month - 1]) {
-    return false;
-  }
-
-  return true;
-}
-
-function isLeapYear(year: number) {
-  return year % 4 === 0;
+  return isValid(parsed);
 }
