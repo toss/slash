1e84fbe696b28748ce3530a203fcc12c676b69b3
bosung baek <teabs1125@gmail.com>
2024-03-30 03:10:02 +0900

    test(@toss/utils): add isHexToRgba test code

--- packages/common/utils/src/hexToRgba.spec.ts
+++ packages/common/utils/src/hexToRgba.spec.ts
@@ -6,6 +6,14 @@ describe('utils.hexToRgba', () => {
       expect(hexToRgba('#ffffff')).toEqual('rgba(255,255,255,1)');
     });
 
+    test('e3e3e3 -> rgba(227,227,227,1)', () => {
+      expect(hexToRgba('#e3e3e3')).toEqual('rgba(227,227,227,1)');
+    });
+
+    test('"e3e3e33"는 잘못된 형식이다)', () => {
+      expect(() => hexToRgba('e3e3e33')).toThrowError();
+    });
+
     test('#17171c -> rgba(23,23,28,1)', () => {
       expect(hexToRgba('#17171c')).toEqual('rgba(23,23,28,1)');
     });
