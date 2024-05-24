4beda7bbcd130a806bb280c3d0b662ed7ae92fd1
Collection50 <fkaus65@naver.com>
2024-05-24 16:32:18 +0900

    refactor: modify type to respond to all react html tag and prevent errors by the developer

--- packages/react/react/src/components/OutsideClick/OutsideClick.tsx
+++ packages/react/react/src/components/OutsideClick/OutsideClick.tsx
@@ -1,17 +1,41 @@
-import { ComponentProps, ElementType, ReactNode, useState } from 'react';
+import { ComponentProps, ElementType, PropsWithChildren, ReactNode, useState } from 'react';
 import { useOutsideClickEffect } from '../../index';
 
-type OutsideClickProp<Tag extends ElementType> = ComponentProps<Tag> & { children: ReactNode } & {
-  as?: Tag;
-  callback: () => void;
-};
+type NonHaveChildElements =
+  | 'input'
+  | 'textarea'
+  | 'img'
+  | 'br'
+  | 'hr'
+  | 'meta'
+  | 'link'
+  | 'base'
+  | 'col'
+  | 'embed'
+  | 'source'
+  | 'track'
+  | 'wbr';
 
-export function OutsideClick<E extends HTMLElement = HTMLDivElement, Tag extends ElementType = 'div'>({
-  as,
-  children,
-  callback,
-  ...props
-}: OutsideClickProp<Tag>) {
+type NoChildren<Tag extends ElementType> = Tag extends NonHaveChildElements
+  ? { children?: never }
+  : { children?: ReactNode };
+
+type AsRequired<Tag extends ElementType> = Tag extends 'div' ? { as?: Tag } : { as: Tag };
+
+type AllowedTagName<Tag extends ElementType> = Tag extends keyof HTMLElementTagNameMap
+  ? HTMLElementTagNameMap[Tag]
+  : HTMLElement;
+
+type OutsideClickProp<Tag extends ElementType> = PropsWithChildren<ComponentProps<Tag>> &
+  AsRequired<Tag> &
+  NoChildren<Tag> & {
+    callback: () => void;
+  };
+
+export default function OutsideClick<
+  Tag extends ElementType = 'div',
+  E extends AllowedTagName<Tag> = AllowedTagName<Tag>
+>({ as, children, callback, ...props }: OutsideClickProp<Tag>) {
   const [element, setElement] = useState<E | null>(null);
 
   useOutsideClickEffect(element, callback);
