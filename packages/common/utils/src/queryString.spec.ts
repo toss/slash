98abeb21df7138185cf402288916039eca4528f1
Minsu <52266597+Gaic4o@users.noreply.github.com>
2024-05-31 17:54:44 +0900

    fix(@toss/utils): added improvements and test code to the queryString function. (#455)
    * docs(@toss/utils): translate queryString comments to English
    
    * docs(@toss/utils): translate queryString test code to English
    
    * fix(@toss/utils): use QS module for queryString function calls
    
    * test(@toss/utils): add QS.get test cases for queryString
    
    ---------
    
    Co-authored-by: Sojin Park <raon0211@toss.im>

--- packages/common/utils/src/queryString.spec.ts
+++ packages/common/utils/src/queryString.spec.ts
@@ -1,18 +1,18 @@
-import { createQueryString, createSearchParamString, parseQueryString, QS } from './queryString';
+import { QS } from './queryString';
 
-describe('parseQueryString은', () => {
-  it('정상적으로 query string을 파싱한다.', () => {
-    const result = parseQueryString<{ foo: string; bar: string }>('?foo=bar&bar=baz');
+describe('QS.parse', () => {
+  it('should correctly parse a query string.', () => {
+    const result = QS.parse<{ foo: string; bar: string }>('?foo=bar&bar=baz');
 
     expect(result.foo).toEqual('bar');
     expect(result.bar).toEqual('baz');
   });
 
-  it('정상적으로 인코딩된 query string을 파싱한다.', () => {
+  it('should correctly parse an encoded query string.', () => {
     const url = 'https://toss.im';
 
-    const result = parseQueryString<{ url: string }>(
-      createQueryString({
+    const result = QS.parse<{ url: string }>(
+      QS.create({
         url: url,
       })
     );
@@ -23,14 +23,14 @@ describe('parseQueryString은', () => {
   /**
    * @see https://stackoverflow.com/questions/31670413/plus-sign-in-encoded-url
    */
-  it('space가 포함된 문자열을 RFC3986에 맞게 잘 디코딩한다', () => {
-    const result = parseQueryString<{ prop1: string }>('?prop1=%EC%A0%95%EC%B9%98%20%ED%9B%84%EC%9B%90%EA%B8%88');
+  it('should correctly decode a string with spaces according to RFC3986.', () => {
+    const result = QS.parse<{ prop1: string }>('?prop1=%EC%A0%95%EC%B9%98%20%ED%9B%84%EC%9B%90%EA%B8%88');
 
     expect(result.prop1).toEqual('정치 후원금');
   });
 
-  it('space가 포함된 문자열의 배열을 RFC3986에 맞게 잘 디코딩한다', () => {
-    const result = parseQueryString<{ prop4: string[] }>(
+  it('should correctly decode an array of strings with spaces according to RFC3986.', () => {
+    const result = QS.parse<{ prop4: string[] }>(
       '?prop4=%EC%A0%95%EC%B9%98%20%ED%9B%84%EC%9B%90%EA%B8%88&prop4=%EB%A7%88%EC%9D%B4%20%EB%8D%B0%EC%9D%B4%ED%84%B0'
     );
     expect(result.prop4).toHaveLength(2);
@@ -38,8 +38,8 @@ describe('parseQueryString은', () => {
     expect(result.prop4).toContain('마이 데이터');
   });
 
-  it('+가 포함된 문자열을 RFC3986에 맞게 잘 인코딩한다', () => {
-    const result = parseQueryString<{ prop1: string }>('?prop1=%EC%A0%95%EC%B9%98%2B%ED%9B%84%EC%9B%90%EA%B8%88');
+  it('should correctly encode a string containing a + according to RFC3986.', () => {
+    const result = QS.parse<{ prop1: string }>('?prop1=%EC%A0%95%EC%B9%98%2B%ED%9B%84%EC%9B%90%EA%B8%88');
     expect(result.prop1).toEqual('정치+후원금');
   });
 });
@@ -51,32 +51,32 @@ interface Params {
   prop4?: string[];
 }
 
-describe('createQueryString은', () => {
-  it('빈 객체를 받으면 빈 문자열을 반환한다.', () => {
+describe('QS.create', () => {
+  it('should return an empty string when given an empty object.', () => {
     const params: Params = {};
-    const result = createQueryString(params);
+    const result = QS.create(params);
 
     expect(result).toEqual('');
   });
 
-  it('object를 받으면 정상적인 queryString을 반환한다.', () => {
+  it('should return a valid queryString when given an object.', () => {
     const params: Params = {
       prop1: 'prop1',
       prop2: 2,
     };
-    const result = createQueryString(params);
+    const result = QS.create(params);
 
     expect(result).toEqual('?prop1=prop1&prop2=2');
   });
 
-  it('배열을 받으면 정상적인 queryString을 반환한다.', () => {
+  it('should return a valid queryString when given an array.', () => {
     const params: Params = {
       prop1: 'prop1',
       prop2: 2,
       prop3: undefined,
       prop4: ['a', 'b'],
     };
-    const result = createQueryString(params);
+    const result = QS.create(params);
 
     expect(result).toEqual('?prop1=prop1&prop2=2&prop4=a&prop4=b');
   });
@@ -84,81 +84,63 @@ describe('createQueryString은', () => {
   /**
    * @see https://stackoverflow.com/questions/31670413/plus-sign-in-encoded-url
    */
-  it('space가 포함된 문자열을 RFC3986에 맞게 잘 인코딩한다', () => {
+  it('should correctly encode a string with spaces according to RFC3986.', () => {
     const params: Params = {
       prop1: '정치 후원금',
     };
-    const result = createQueryString(params);
+    const result = QS.create(params);
 
     expect(result).toEqual('?prop1=%EC%A0%95%EC%B9%98%20%ED%9B%84%EC%9B%90%EA%B8%88');
   });
 
-  it('space가 포함된 문자열의 배열을 RFC3986에 맞게 잘 인코딩한다', () => {
+  it('should correctly encode an array of strings with spaces according to RFC3986.', () => {
     const params: Params = {
       prop4: ['정치 후원금', '마이 데이터'],
     };
-    const result = createQueryString(params);
+    const result = QS.create(params);
 
     expect(result).toEqual(
       '?prop4=%EC%A0%95%EC%B9%98%20%ED%9B%84%EC%9B%90%EA%B8%88&prop4=%EB%A7%88%EC%9D%B4%20%EB%8D%B0%EC%9D%B4%ED%84%B0'
     );
   });
 
-  it('+가 포함된 문자열을 RFC3986에 맞게 잘 인코딩한다', () => {
+  it('should correctly encode a string containing + according to RFC3986.', () => {
     const params: Params = {
       prop1: '정치+후원금',
     };
-    const result = createQueryString(params);
+    const result = QS.create(params);
 
     expect(result).toEqual('?prop1=%EC%A0%95%EC%B9%98%2B%ED%9B%84%EC%9B%90%EA%B8%88');
   });
 });
 
-describe('createSearchParamString은', () => {
-  it('전달받은 params을 프로토콜 규약에 맞게 파싱한다.', () => {
-    const params = {
-      foo: '1',
-      bar: 2,
-      baz: ['a', 'b', 'c'],
-    };
-
-    const result = createSearchParamString(params);
-
-    expect(result).toEqual('foo=1&bar=2&baz=a&baz=b&baz=c');
+describe('QS.get', () => {
+  beforeEach(() => {
+    QS.parse = jest.fn().mockImplementation(() => ({
+      prop1: 'prop1',
+      prop2: 'prop2',
+      prop3: '40',
+    }));
   });
 
-  it('space가 포함된 문자열을 RFC3986에 맞게 잘 인코딩한다', () => {
-    const params: Params = {
-      prop1: '정치 후원금',
-    };
-    const result = createQueryString(params);
-
-    expect(result).toEqual('?prop1=%EC%A0%95%EC%B9%98%20%ED%9B%84%EC%9B%90%EA%B8%88');
+  it('should correctly return an existing single string value', () => {
+    const name = QS.get('prop1');
+    expect(name).toEqual('prop1');
   });
 
-  it('space가 포함된 문자열의 배열을 RFC3986에 맞게 잘 인코딩한다', () => {
-    const params: Params = {
-      prop4: ['정치 후원금', '마이 데이터'],
-    };
-    const result = createQueryString(params);
-
-    expect(result).toEqual(
-      '?prop4=%EC%A0%95%EC%B9%98%20%ED%9B%84%EC%9B%90%EA%B8%88&prop4=%EB%A7%88%EC%9D%B4%20%EB%8D%B0%EC%9D%B4%ED%84%B0'
-    );
+  it('should return undefined for a non-existing key', () => {
+    const result = QS.get('prop4');
+    expect(result).toBeUndefined();
   });
 
-  it('+가 포함된 문자열을 RFC3986에 맞게 잘 인코딩한다', () => {
-    const params: Params = {
-      prop1: '정치+후원금',
-    };
-    const result = createQueryString(params);
-
-    expect(result).toEqual('?prop1=%EC%A0%95%EC%B9%98%2B%ED%9B%84%EC%9B%90%EA%B8%88');
+  it('should apply a parser function to convert a string to a number', () => {
+    const age = QS.get('prop3', Number);
+    expect(age).toEqual(40);
   });
 });
 
 describe('QS.set', () => {
-  it('쿼리 파라미터를 RFC 3986에 따라 설정한다.', () => {
+  it('should set query parameters according to RFC 3986.', () => {
     expect(QS.set('?with_space=hi%20hi', 'referrer', 'foo')).toMatchInlineSnapshot(
       `"?with_space=hi%20hi&referrer=foo"`
     );
