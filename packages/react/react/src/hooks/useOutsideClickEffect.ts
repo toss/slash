2ef5b0fe38bbe4be1f157cdbfc58455b9f68c7b7
장석원 SeokWon Jang <64577038+tjrdnjs1534@users.noreply.github.com>
2024-05-31 17:49:50 +0900

    fix(@toss/react): add useOutsideClickEffect dependency for preventing keep add and remove eventlistener (#453)
    * fix: add callback dependency on useOutsideClickEffect using preservedCallback
    
    * fix: delete touchstart event from `useOutsideClickEffect`
    
    * Revert "fix: delete touchstart event from `useOutsideClickEffect`"
    
    This reverts commit c1b09c9031085149744111b320dbc0ecdf63fd96.
    
    * fix(@toss/validators): Increase the accuracy of verification for isBirthDate6 (#420)
    
    * fix(@toss/validators): Increase the accuracy of verification
    
    * test(@toss/validators): add test case
    
    * fix: catch 2000y bug
    
    * refactor: change from magic number to constant variable
    
    * test: split test case
    
    * refactor: change constant variable name
    
    * refactor(@toss/validators): use proper library(date-fns)
    
    * chore(release): Publish libraries [skip ci]
    
     - @toss/rollup-config@0.2.0
     - docs@1.0.19
     - @toss/assert@1.2.0
     - @toss/crypto@1.2.0
     - @toss/date@1.2.0
     - @toss/hangul@1.7.0
     - @toss/ky@1.2.0
     - @toss/sentry@1.1.0
     - @toss/storage@1.4.0
     - @toss/utility-types@1.2.0
     - @toss/utils@1.5.0
     - @toss/validators@1.3.0
     - @toss/a11y@1.3.0
     - @toss/async-boundary@1.5.0
     - @toss/async-stylesheet@1.2.0
     - @toss/emotion-utils@1.2.0
     - @toss/error-boundary@1.5.0
     - @toss/framer-motion@1.2.0
     - @toss/impression-area@1.4.0
     - @toss/lottie@1.3.0
     - @toss/react-query@1.3.0
     - @toss/react@1.7.0
     - @toss/recoil@1.2.0
     - @toss/redirect@1.3.0
     - @toss/scroll-animation@1.3.0
     - @toss/use-funnel@1.4.0
     - @toss/use-loading@1.2.0
     - @toss/use-overlay@1.4.0
     - @toss/use-query-param@1.3.0
    
    * Revert "chore(release): Publish libraries [skip ci]"
    
    This reverts commit c325c909127dda9ac6d34f5fa5a236966e4c6569.
    
    * Revert "fix(@toss/validators): Increase the accuracy of verification for isBirthDate6 (#420)"
    
    This reverts commit 2565f17071af08cc259a56686c5996331d18667a.
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Dongkyu Kim <po4tion0429@gmail.com>
    Co-authored-by: raon0211 <raon0211@toss.im>
    Co-authored-by: Sojin Park <raon0211@gmail.com>

--- packages/react/react/src/hooks/useOutsideClickEffect.ts
+++ packages/react/react/src/hooks/useOutsideClickEffect.ts
@@ -1,5 +1,6 @@
 import { isNotNil } from '@toss/utils';
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import { usePreservedCallback } from './usePreservedCallback';
 
 type OneOrMore<T> = T | T[];
 
@@ -7,24 +8,22 @@ type OneOrMore<T> = T | T[];
 export function useOutsideClickEffect(container: OneOrMore<HTMLElement | null>, callback: () => void) {
   const containers = useRef<HTMLElement[]>([]);
 
-  const handleDocumentClick = useCallback(
-    ({ target }: MouseEvent | TouchEvent) => {
-      if (target === null) {
-        return;
-      }
 
-      if (containers.current.length === 0) {
-        return;
-      }
+  const handleDocumentClick = usePreservedCallback(({ target }: MouseEvent | TouchEvent) => {
+    if (target === null) {
+      return;
+    }
 
-      if (containers.current.some(x => x.contains(target as Node))) {
-        return;
-      }
+    if (containers.current.length === 0) {
+      return;
+    }
 
-      callback();
-    },
-    [callback]
-  );
+    if (containers.current.some(x => x.contains(target as Node))) {
+      return;
+    }
+
+    callback();
+  });
 
   useEffect(() => {
     containers.current = [container].flat(1).filter(isNotNil);
