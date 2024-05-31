38537845e586556863f0c126e63da1aea7aa0907
BOSUNG BAEK <78058734+BO-LIKE-CHICKEN@users.noreply.github.com>
2024-05-31 18:02:09 +0900

    fix(@toss/utils): correct hex validation to prevent unexpected color parsing (#458)
    * test(@toss/utils): add isHexToRgba test code
    
    * test(@toss/utils): add isHexToRgba test code
    
    * fix(@toss/utils): correct hex validation to prevent unexpected color parsing
    
    * fix(@toss/utils): correct typo
    
    * test(@toss/utils): reorganize hex validation tests for clarity
    
    * test(@toss/utils): adjust tests for hex value formats, remove Korean comments
    
    * refactor(@toss/utils): improve hexToRgba function clarity using regex
    
    ---------
    
    Co-authored-by: Sojin Park <raon0211@toss.im>

--- packages/common/utils/src/hexToRgba.ts
+++ packages/common/utils/src/hexToRgba.ts
@@ -3,6 +3,11 @@ function parseHexValueStr(str: string) {
   return parseInt(str, 16);
 }
 
+export function isValidHexValue(hex: string) {
+  const regex = /^#?[0-9A-Fa-f]{6}$/;
+  return regex.test(hex);
+}
+
 export function isRGBDecimalValue(rgbDecimal: number) {
   return 0 <= rgbDecimal && rgbDecimal <= 255;
 }
@@ -12,19 +17,19 @@ export function isAlphaValue(alpha: number) {
 }
 
 export function hexToRgba(hex: string, alpha = 1) {
+  if (!isValidHexValue(hex)) {
+    throw new Error(`Invalid hex value: ${hex}`);
+  }
+
   if (!isAlphaValue(alpha)) {
-    throw new Error(`잘못된 알파값 입니다(0~1): ${alpha}`);
+    throw new Error(`Invalid alpha value(0~1): ${alpha}`);
   }
 
-  hex = hex.length === 7 ? hex.slice(1) : hex;
+  const normalizedHex = hex.startsWith('#') ? hex.slice(1) : hex;
 
-  const r = parseHexValueStr(hex.slice(0, 2));
-  const g = parseHexValueStr(hex.slice(2, 4));
-  const b = parseHexValueStr(hex.slice(4, 6));
-
-  if ([r, g, b].some(x => Number.isNaN(x) || !isRGBDecimalValue(x))) {
-    throw new Error(`잘못된 hex값 입니다.: ${hex}`);
-  }
+  const r = parseHexValueStr(normalizedHex.slice(0, 2));
+  const g = parseHexValueStr(normalizedHex.slice(2, 4));
+  const b = parseHexValueStr(normalizedHex.slice(4, 6));
 
   return `rgba(${r},${g},${b},${alpha})`;
 }
