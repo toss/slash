17e900dddec43c3404da9bc70cefe07b04f12a7c
gildong <rlfehd2013@naver.com>
2024-06-03 14:13:38 +0900

    feat(@toss/react): Support stop & resume in useInterval (#476)
    * feat: stop, resume, intervalRunning State
    
    * fix: change to use the enable option rather than useState
    
    * feat: not register when enabled is false

--- packages/react/react/src/hooks/useInterval.ko.md
+++ packages/react/react/src/hooks/useInterval.ko.md
@@ -11,6 +11,8 @@ type IntervalOptions =
       delay: number | null;
       // 만약 false로 지정된 경우 Effect를 즉시 실행시킵니다.
       trailing?: boolean;
+      // 만약 false인 지정된 경우, Effect가 수행되지 않습니다.
+      enabled?: boolean;
     };
 ```
 
--- packages/react/react/src/hooks/useInterval.ts
+++ packages/react/react/src/hooks/useInterval.ts
@@ -7,32 +7,36 @@ type IntervalOptions =
   | {
       delay: number | null;
       trailing?: boolean;
+      enabled?: boolean;
     };
+
+const getEnabled = (options: IntervalOptions) => {
+  if (typeof options === 'number' || options.enabled === undefined) return true;
+  return options.enabled;
+};
 
 /** @tossdocs-ignore */
 export function useInterval(callback: () => void, options: IntervalOptions) {
   const delay = typeof options === 'number' ? options : options.delay;
   const trailing = typeof options === 'number' ? undefined : options.trailing;
+  const enabled = getEnabled(options);
+
   const savedCallback = usePreservedCallback(callback ?? noop);
 
   useEffect(() => {
-    if (trailing === false) {
+    if (trailing === false && enabled) {
       savedCallback();
     }
   }, [trailing, savedCallback]);
 
   useEffect(() => {
-    if (delay === null) {
+    if (delay === null || !enabled) {
       return () => {
         return;
       };
     }
 
-    function tick() {
-      savedCallback();
-    }
-
-    const id = window.setInterval(tick, delay);
+    const id = window.setInterval(savedCallback, delay);
     return () => window.clearInterval(id);
-  }, [delay, savedCallback]);
+  }, [delay, savedCallback, enabled]);
 }
