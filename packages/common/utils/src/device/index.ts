b975d9b452f4393f19672d788810491ce631db5b
jinwoo0103 <jinwoo0103@kaist.ac.kr>
2025-04-30 22:03:16 +0900

    fix(utils): export missing functions from @toss/utils/device

--- packages/common/utils/src/device/index.ts
+++ packages/common/utils/src/device/index.ts
@@ -1,7 +1,9 @@
 /** @tossdocs-ignore */
 export * from './getOSByUserAgent';
+export * from './isAndroid';
 export * from './isClient';
 export * from './isIE';
+export * from './isIOS';
 export * from './isMacOS';
 export * from './isMobileWeb';
 export * from './isServer';
